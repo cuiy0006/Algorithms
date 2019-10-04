6cd956534d7b61cec0fdc21eb544d6c1fbd0b35f
Yao Cui <yc2554@nyu.edu>
2019-10-03 23:13:06 -0400

    Create 279. Perfect Squares.cpp

---
+++ leetcode/279. Perfect Squares.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        vector<int> v(n + 1, n + 1);
+        v[0] = 0;
+        for(int i = 1; i <= n; ++i){
+            for(int j = 1; j <= sqrt(i); ++j){
+                v[i] = min(v[i], v[i - j * j] + 1);
+            }
+        }
+        return v[n];
+    }
+};
