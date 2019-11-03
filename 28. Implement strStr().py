166a247c29ac2c24b130d7533af9f712debbd8d6
Yao Cui <yc2554@nyu.edu>
2019-11-03 03:46:27 -0500

    Create 28. Implement strStr().py

---
+++ 28. Implement strStr().py
@@ -0,0 +1,8 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if needle == '':
+            return 0
+        for i in range(len(haystack)-len(needle)+1):
+            if needle == haystack[i:i+len(needle)]:
+                return i
+        return -1
