05cbce8c2860091fa300aebb04fb5fc80b582c58
Yao Cui <yc2554@nyu.edu>
2019-12-09 23:31:02 -0500

    Create 189. Rotate Array.py

---
+++ leetcode/189. Rotate Array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        def reverse_lst(i, j):
+            while i < j:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+                j -= 1
+        
+        k = k % len(nums)
+        reverse_lst(len(nums) - k, len(nums) - 1)
+        reverse_lst(0, len(nums) - k - 1)
+        reverse_lst(0, len(nums) - 1)
