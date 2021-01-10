ac599a27df1cb6f5be95c82835f288ca638dd1d7
Yao Cui <yc2554@nyu.edu>
2021-01-09 19:08:32 -0500

    Update 455. Assign Cookies.cpp

--- leetcode/455. Assign Cookies.cpp
+++ leetcode/455. Assign Cookies.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        std::sort(g.begin(), g.end());
-        std::sort(s.begin(), s.end());
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
         int i = 0;
         int j = 0;
         
-        while(i < g.size() && j < s.size()){
-            if(g[i] <= s[j]){
+        while(i < g.size() && j < s.size()) {
+            if(s[j] >= g[i]){
                 ++i;
-                ++j;
-            } else {
-                ++j;
             }
+            ++j;
         }
+        
         return i;
     }
 };
