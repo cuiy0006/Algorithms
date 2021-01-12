f4b2c3c021599580ff047ac42e5ab68a142077d4
Yao Cui <yc2554@nyu.edu>
2021-01-12 02:50:13 -0500

    Update 633. Sum of Square Numbers.py

--- leetcode/633. Sum of Square Numbers.py
+++ leetcode/633. Sum of Square Numbers.py
@@ -1,11 +1,10 @@
 class Solution:
-    def judgeSquareSum(self, c):
-        """
-        :type c: int
-        :rtype: bool
-        """
-        for a in range(0, int(c**0.5) + 1):
-            b = int((c - a ** 2) ** 0.5)
-            if b ** 2 + a ** 2 == c:
+    def judgeSquareSum(self, c: int) -> bool:
+        for a in range(0, int(c ** 0.5) + 1):
+            b = (c - a ** 2) ** 0.5
+            if int(b) == b:
                 return True
+            if b <= a:
+                return False
         return False
+        
