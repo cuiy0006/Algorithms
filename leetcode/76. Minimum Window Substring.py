2b73c90a966cdeebc316633008b43a384fcd886a
Yao Cui <yc2554@nyu.edu>
2021-01-12 02:29:26 -0500

    Update 76. Minimum Window Substring.py

--- leetcode/76. Minimum Window Substring.py
+++ leetcode/76. Minimum Window Substring.py
@@ -1,25 +1,26 @@
-class Solution(object):
-    def minWindow(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: str
-        """
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
         i, j, start, end = 0, 0, 0, 0
         minlen = sys.maxsize
         cnt = len(t)
-        dic = {c:0 for c in s}
+        dic = {}
         for c in t:
             if c in dic:
                 dic[c] += 1
             else:
                 dic[c] = 1
         while j < len(s):
+            if s[j] not in dic:
+                j += 1
+                continue
             if dic[s[j]] > 0:
                 cnt -= 1
             dic[s[j]] -= 1
             j += 1
             while cnt == 0:
+                if s[i] not in dic:
+                    i += 1
+                    continue
                 if j - i < minlen:
                     minlen = j - i
                     start = i
@@ -29,6 +30,7 @@ class Solution(object):
                 dic[s[i]] += 1
                 i += 1
         return s[start:end]
+
 
     
     
