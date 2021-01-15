e204c1f108da7148d45777328a23bf921d9acda3
Yao Cui <yc2554@nyu.edu>
2021-01-15 02:01:13 -0500

    Update 148. Sort List.py

--- leetcode/148. Sort List.py
+++ leetcode/148. Sort List.py
@@ -1,5 +1,87 @@
 # Definition for singly-linked list.
 # class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def sortList(self, head: ListNode) -> ListNode:
+        def helper(node):
+            if node == None or node.next == None:
+                return (node, node)
+            
+            lleft = lright = ListNode()
+            rleft = rright = ListNode()
+            
+            last = None
+            slow = fast = node
+            while True:
+                if fast == None or fast.next == None:
+                    last.next = slow.next
+                    last = slow
+                    break
+                    
+                fast = fast.next.next
+                last = slow
+                slow = slow.next
+            
+            mleft = mright = last
+            p = node
+            
+            while p != None:
+                if p.val == mleft.val:
+                    mright.next = p
+                    mright = mright.next
+                elif p.val > mleft.val:
+                    rright.next = p
+                    rright = rright.next
+                else:
+                    lright.next = p
+                    lright = lright.next
+                p = p.next
+            
+            mright.next = lright.next = rright.next = None
+            
+            lleft, lright = helper(lleft.next)
+            rleft, rright = helper(rleft.next)
+            
+            left = lleft
+            if lleft == None:
+                left = mleft
+            else:
+                lright.next = mleft
+            
+            mright.next = rleft
+            right = rright if rright != None else mright
+            return (left, right)
+        
+        return helper(head)[0]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Definition for singly-linked list.
+# class ListNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
