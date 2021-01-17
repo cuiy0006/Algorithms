0062d8e8f1acacd88abaeb63257d6337f4a9b38e
Yao Cui <yc2554@nyu.edu>
2021-01-17 05:13:27 -0500

    Update 540. Single Element in a Sorted Array.py

--- leetcode/540. Single Element in a Sorted Array.py
+++ leetcode/540. Single Element in a Sorted Array.py
@@ -1,23 +1,22 @@
-class Solution(object):
-    def singleNonDuplicate(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        left = 0
-        right = len(nums) - 1
-        while left <= right:
-            mid = (left + right) // 2
-            if mid == 0 or mid == len(nums) - 1 or nums[mid] != nums[mid + 1] and nums[mid] != nums[mid - 1]:
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        i = 0
+        j = len(nums) - 1
+        
+        while i < j:
+            mid = (i + j) // 2
+            if mid == 0 or mid == len(nums) - 1:
                 return nums[mid]
-            
+
             if mid % 2 == 1:
-                if nums[mid] == nums[mid + 1]:
-                    right = mid - 1    
-                elif nums[mid] == nums[mid - 1]:
-                    left = mid + 1
+                if nums[mid] == nums[mid - 1]:
+                    i = mid + 1
+                else:
+                    j = mid
             else:
                 if nums[mid] == nums[mid + 1]:
-                    left = mid + 2    
-                elif nums[mid] == nums[mid - 1]:
-                    right = mid - 2
+                    i = mid + 1
+                else:
+                    j = mid
+        
+        return nums[i]
