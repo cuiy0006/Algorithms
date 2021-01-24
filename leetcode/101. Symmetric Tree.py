aeea641b46fb2910a333a75e7b8d74adf726f209
Yao Cui <yc2554@nyu.edu>
2021-01-23 20:09:10 -0500

    Update 101. Symmetric Tree.py

--- leetcode/101. Symmetric Tree.py
+++ leetcode/101. Symmetric Tree.py
@@ -1,28 +1,22 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
-class Solution(object):
-    def isSymmetric(self, root):
-        """
-        :type root: TreeNode
-        :rtype: bool
-        """
-        if root is None:
-            return True
-        
-        def helper(first, second):
-            if first is None and second is None:
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        def helper(left, right):
+            if left == None and right == None:
                 return True
-            if first is None and second is not None:
+            elif right == None or left == None:
                 return False
-            if first is not None and second is None:
+            
+            if left.val != right.val:
                 return False
-            if first.val != second.val:
-                return False
-            return helper(first.left, second.right) and helper(first.right, second.left)
+            
+            return helper(left.left, right.right) and helper(left.right, right.left)
         
+        if root == None:
+            return True
         return helper(root.left, root.right)
