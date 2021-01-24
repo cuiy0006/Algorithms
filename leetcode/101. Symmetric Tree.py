a5428fb18779bd74bf76985ec39a18803e17d08c
Yao Cui <yc2554@nyu.edu>
2021-01-23 20:36:11 -0500

    Update 101. Symmetric Tree.py

--- leetcode/101. Symmetric Tree.py
+++ leetcode/101. Symmetric Tree.py
@@ -20,3 +20,39 @@ class Solution:
         if root == None:
             return True
         return helper(root.left, root.right)
+
+    
+    
+    
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        if root == None:
+            return True
+
+        left = [root.left]
+        right = [root.right]
+        
+        while len(left) != 0:
+            lnode = left.pop()
+            rnode = right.pop()
+            
+            if lnode == None and rnode == None:
+                continue
+            elif lnode == None or rnode == None:
+                return False
+            
+            if lnode.val != rnode.val:
+                return False
+
+            left.append(lnode.left)
+            right.append(rnode.right)
+            left.append(lnode.right)
+            right.append(rnode.left)
+        
+        return True
