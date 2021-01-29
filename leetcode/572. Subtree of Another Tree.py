fa7c4358c837685e57627210db443121e48ab5b0
Yao Cui <yc2554@nyu.edu>
2021-01-29 02:25:12 -0500

    Update 572. Subtree of Another Tree.py

--- leetcode/572. Subtree of Another Tree.py
+++ leetcode/572. Subtree of Another Tree.py
@@ -1,22 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def isSubtree(self, s, t):
-        """
-        :type s: TreeNode
-        :type t: TreeNode
-        :rtype: bool
-        """
-        if s == None:
-            return False
-        return self.isSameTree(s, t) or self.isSubtree(s.left, t) or self.isSubtree(s.right, t)
-        
-    def isSameTree(self, x, y):
-        if x == None and y == None:
+    def isSubtree(self, s: TreeNode, t: TreeNode) -> bool:
+        lst = [s]
+        while len(lst) != 0:
+            node = lst.pop()
+            if self.isSame(node, t):
+                return True
+            if node.right != None:
+                lst.append(node.right)
+            if node.left != None:
+                lst.append(node.left)
+        return False
+    
+    def isSame(self, node1, node2):
+        if node1 == None and node2 == None:
             return True
-        
-        if (x == None and y != None) or (x != None and y == None):
+        elif node1 == None or node2 == None:
             return False
-        
-        if x.val != y.val:
-            return False
-            
-        return self.isSameTree(x.left, y.left) and self.isSameTree(x.right, y.right)
+        return node1.val == node2.val and self.isSame(node1.left, node2.left) and self.isSame(node1.right, node2.right)
