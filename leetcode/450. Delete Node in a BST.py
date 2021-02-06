f2464b2d82941b43c6a78f31fc1651fc7d5380db
Yao Cui <yc2554@nyu.edu>
2021-02-06 02:54:11 -0500

    Create 450. Delete Node in a BST.py

---
+++ leetcode/450. Delete Node in a BST.py
@@ -0,0 +1,55 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def deleteNode(self, root: TreeNode, key: int) -> TreeNode:
+        node = root
+        parent = None
+        while node is not None and node.val != key:
+            parent = node
+            if node.val > key:
+                node = node.left
+            else:
+                node = node.right
+        if node is None:
+            return root
+
+        if node.left is None and node.right is None:
+            if parent is None:
+                return None
+            else:
+                if parent.left == node:
+                    parent.left = None
+                else:
+                    parent.right = None
+            return root
+        
+        if node.left is not None:
+            parent2 = node
+            node2 = node.left
+            while node2.right is not None:
+                parent2 = node2
+                node2 = node2.right
+            if parent2 == node:
+                node.val = node2.val
+                node.left = node2.left
+            else:
+                node.val = node2.val
+                parent2.right = node2.left
+        elif node.right is not None:
+            parent2 = node
+            node2 = node.right
+            while node2.left is not None:
+                parent2 = node2
+                node2 = node2.left
+            if parent2 == node:
+                node.val = node2.val
+                node.right = node2.right
+            else:
+                node.val = node2.val
+                parent2.left = node2.right
+        return root
+        
