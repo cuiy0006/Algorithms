7784d6c849c0d2a5e791e4ad1ba325e956a8b286
Yao Cui <yc2554@nyu.edu>
2021-04-26 22:24:40 -0400

    Update 1. Two Sum.py

--- leetcode/1. Two Sum.py
+++ leetcode/1. Two Sum.py
@@ -1,11 +1,10 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        dic = {target-num:i for i, num in enumerate(nums)}
-        for i,num in enumerate(nums):
-            if num in dic and i != dic[num]:
-                return [i, dic[num]]
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dic = {}
+        for i, num in enumerate(nums):
+            num2 = target - num
+            if num2 in dic:
+                return [i, dic[num2]]
+            dic[num] = i
+        
+        return []
