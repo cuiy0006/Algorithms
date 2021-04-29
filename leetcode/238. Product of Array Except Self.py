e2b491804ee59ea60fe78c1fbeabdafcce6bc342
Yao Cui <yc2554@nyu.edu>
2021-04-29 01:32:00 -0400

    Update 238. Product of Array Except Self.py

--- leetcode/238. Product of Array Except Self.py
+++ leetcode/238. Product of Array Except Self.py
@@ -1,17 +1,16 @@
-class Solution(object):
-    def productExceptSelf(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
-        if len(nums) == 0:
-            return []
-        res = [1] * len(nums)
-        for i in range(1, len(nums)):
-            res[i] = res[i-1] * nums[i-1]
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
         curr = 1
-        for i in range(len(nums)-1, -1, -1):
-            if i != len(nums) - 1:
-                res[i] = res[i] * curr
-            curr = curr * nums[i]
+        res = []
+        for num in nums:
+            res.append(curr)
+            curr *= num
+        
+        curr = 1
+        i = len(nums) - 1
+        while i >= 0:
+            res[i] = curr * res[i]
+            curr *= nums[i]
+            i -= 1
+        
         return res
