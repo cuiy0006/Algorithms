8a1349cadefae1d5e6a0bb348bc4e5bb1a0297a3
Yao Cui <yc2554@nyu.edu>
2021-05-10 19:26:36 -0400

    Update 680. Valid Palindrome II.py

--- leetcode/680. Valid Palindrome II.py
+++ leetcode/680. Valid Palindrome II.py
@@ -1,23 +1,20 @@
 class Solution:
-    def validPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        def isValid(sub):
-            i = 0
-            j = len(sub) - 1
+    def validPalindrome(self, s: str) -> bool:
+        def is_palindrome(s, i, j):
             while i < j:
-                if sub[i] != sub[j]:
+                if s[i] != s[j]:
                     return False
                 i += 1
                 j -= 1
             return True
+        
         i = 0
         j = len(s) - 1
+        
         while i < j:
             if s[i] != s[j]:
-                return isValid(s[i:j]) or isValid(s[i+1:j+1])
+                return is_palindrome(s, i + 1, j) or is_palindrome(s, i, j - 1)
+            
             i += 1
             j -= 1
         return True
