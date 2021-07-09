9d4e877dc4fcd8476c8fdf6718cb5307871be0bc
Yao Cui <yc2554@nyu.edu>
2021-07-09 00:14:55 -0400

    Create 934. Shortest Bridge.py

---
+++ leetcode/934. Shortest Bridge.py
@@ -0,0 +1,50 @@
+from collections import deque
+
+class Solution:
+    def shortestBridge(self, grid: List[List[int]]) -> int:
+        q = deque()
+        
+        def find_island(x, y):
+            if x < 0 or x > len(grid) - 1 or y < 0 or y > len(grid[0]) - 1:
+                return
+            
+            if grid[x][y] != 1:
+                return
+            
+            grid[x][y] = '#'
+            q.append((x, y, 0))
+            find_island(x + 1, y)
+            find_island(x - 1, y)
+            find_island(x, y + 1)
+            find_island(x, y - 1)
+        
+
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if grid[i][j] == 1:
+                    find_island(i, j)
+                    break
+            else:
+                continue
+            break
+            
+        distances = [[None for i in range(len(grid[0]))] for j in range(len(grid))]
+        dirs = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        
+        while len(q) != 0:
+            x, y, curr = q.popleft()
+            
+            if x < 0 or x > len(grid) - 1 or y < 0 or y > len(grid[0]) - 1:
+                continue
+            
+            if grid[x][y] == 1:
+                return curr - 1
+            
+            if distances[x][y] is None:
+                distances[x][y] = curr
+            else:
+                continue
+            
+            for dx, dy in dirs:
+                q.append((x + dx, y + dy, curr + 1))
+                
