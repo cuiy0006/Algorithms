15cd90b56c01c2d115fcc96f4b7ed84dce292ea6
Yao Cui <yc2554@nyu.edu>
2021-08-03 21:10:55 -0400

    Update 438. Find All Anagrams in a String.py

--- leetcode/438. Find All Anagrams in a String.py
+++ leetcode/438. Find All Anagrams in a String.py
@@ -1,71 +1,28 @@
-#better
-class Solution(object):
-    def findAnagrams(self, s, p):
-        """
-        :type s: str
-        :type p: str
-        :rtype: List[int]
-        """
-        res = []
-        dic = [0 for i in range(26)]
-        for c in p:
-            dic[ord(c)-ord('a')] += 1
-        left=right=0
-        cnt = len(p)
-        while right < len(s):
-            if dic[ord(s[right])-ord('a')] > 0:
-                cnt -= 1
-            dic[ord(s[right])-ord('a')] -= 1
-            if right - left == len(p):
-                if dic[ord(s[left])-ord('a')] >= 0:
-                    cnt += 1
-                dic[ord(s[left])-ord('a')] += 1
-                left += 1
-            if cnt == 0:
-                res.append(left)
-            right += 1
-        return res
-        
-        
-
-#mine
-class Solution(object):
-    def findAnagrams(self, s, p):
-        """
-        :type s: str
-        :type p: str
-        :rtype: List[int]
-        """
-        def isSame(pdic, dic):
-            for k,v in pdic.items():
-                if k not in dic or dic[k] != v:
-                    return False
-            return True
-        
-        if len(s) < len(p):
-            return []
-        res = []
-        pdic = {}
-        for c in p:
-            if c in pdic:
-                pdic[c] += 1
-            else:
-                pdic[c] = 1
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
         dic = {}
-        for i in range(len(p)):
-            if s[i] in dic:
-                dic[s[i]] += 1
-            else:
-                dic[s[i]] = 1
-        if isSame(pdic, dic):
-            res.append(0)
+        for c in p:
+            if c not in dic:
+                dic[c] = 0
+            dic[c] += 1
+        n = len(p)
+        res = []
+        
+        for i, c in enumerate(s):
+            j = i - len(p)
+            if i >= len(p):
+                if s[j] in dic:
+                    dic[s[j]] += 1
+                    if dic[s[j]] > 0:
+                        n += 1
+            
+            if c in dic:
+                dic[c] -= 1
+                if dic[c] >= 0:
+                    n -= 1
+
+            if n == 0:
+                res.append(i - len(p) + 1)
                 
-        for i in range(len(p), len(s)):
-            dic[s[i-len(p)]]-=1
-            if s[i] in dic:
-                dic[s[i]] += 1
-            else:
-                dic[s[i]] = 1
-            if isSame(pdic, dic):
-                res.append(i-len(p)+1)
         return res
+            
