79079717746e1caf0a7f3b2392a56706981497dc
Yao Cui <yc2554@nyu.edu>
2021-11-29 23:51:38 -0500

    Update 244. Shortest Word Distance II.py

--- leetcode/244. Shortest Word Distance II.py
+++ leetcode/244. Shortest Word Distance II.py
@@ -30,3 +30,40 @@ class WordDistance:
 # Your WordDistance object will be instantiated and called as such:
 # obj = WordDistance(wordsDict)
 # param_1 = obj.shortest(word1,word2)
+
+
+
+class WordDistance:
+
+    def __init__(self, wordsDict: List[str]):
+        self.word_to_pos_list = {}
+        for pos, word in enumerate(wordsDict):
+            if word not in self.word_to_pos_list:
+                self.word_to_pos_list[word] = []
+            self.word_to_pos_list[word].append(pos)
+
+    def shortest(self, word1: str, word2: str) -> int:
+        pos_list1 = self.word_to_pos_list[word1]
+        pos_list2 = self.word_to_pos_list[word2]
+        i = j = 0
+        res = sys.maxsize
+        while i < len(pos_list1) or j < len(pos_list2):
+            res = min(res, abs(pos_list1[i] - pos_list2[j]))
+            if i == len(pos_list1) - 1 and j == len(pos_list2) - 1:
+                break
+            elif i == len(pos_list1) - 1:
+                j += 1
+            elif j == len(pos_list2) - 1:
+                i += 1
+            else:
+                if pos_list1[i] < pos_list2[j]:
+                    i += 1
+                else:
+                    j += 1
+
+        return res
+
+
+# Your WordDistance object will be instantiated and called as such:
+# obj = WordDistance(wordsDict)
+# param_1 = obj.shortest(word1,word2)
