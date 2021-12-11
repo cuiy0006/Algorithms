ad8c44597dd292e0acf150522d232abb8352fb47
Yao Cui <cuiy0006@gmail.com>
2021-12-11 01:43:07 -0500

    Update 68. Text Justification.py

--- leetcode/68. Text Justification.py
+++ leetcode/68. Text Justification.py
@@ -1,46 +1,40 @@
 class Solution:
     def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
-        lines = []
-        line = [words[0]]
-        currlen = len(words[0])
-        
-        for i in range(1, len(words)):
-            word = words[i]
-            if currlen + 1 + len(word) > maxWidth:
-                lines.append(line)
-                line = [word]
-                currlen = len(word)
-            else:
-                line.append(word)
-                currlen += 1 + len(word)
-        
-        if len(line) != 0:
-            lines.append(line)
-        
         res = []
-        
-        for i in range(len(lines)):
-            line = lines[i]
-            if i != len(lines) - 1:
-                if len(line) == 1:
-                    text = line[0] + ' ' * (maxWidth - len(line[0]))
-                else:
-                    length = 0
-                    for word in line:
-                        length += len(word)
-                    
-                    num_space = (maxWidth - length) // (len(line) - 1)
-                    extra_space = (maxWidth - length) % (len(line) - 1)
-                    text = line[0]
-                    for j in range(1, len(line)):
-                        word = line[j]
-                        if extra_space != 0:
-                            text += ' ' * (num_space + 1) + word
-                            extra_space -= 1
-                        else:
-                            text += ' ' * num_space + word
+        i = 0
+        while i < len(words):
+            j = i
+            l = 0
+            while j < len(words):
+                l += len(words[j])
+                if l > maxWidth:
+                    break
+                l += 1
+                j += 1
+                
+            line = ''
+            if j == len(words):
+                line += ' '.join(words[i:])
+                line += ' ' * (maxWidth - len(line))
             else:
-                text = ' '.join(line)
-                text += ' ' * (maxWidth - len(text))
-            res.append(text)
+                cnt = j - i
+                if cnt == 1:
+                    line = words[i]
+                    line += ' ' * (maxWidth - len(line))
+                else:
+                    len_words = sum([len(words[idx]) for idx in range(i, j)])
+                    num_space = (maxWidth - len_words) // (cnt - 1)
+                    extra_space = (maxWidth - len_words) % (cnt - 1)
+
+                    line = ''
+                    for idx in range(i, j - 1):
+                        line += words[idx] + ' ' * num_space
+                        if extra_space != 0:
+                            line += ' '
+                            extra_space -= 1
+                    line += words[j - 1]
+                    
+            res.append(line)
+            i = j
+        
         return res
