3734aae10d823c6c6c3d8f44500058f33d6458a9
Yao Cui <cuiy0006@gmail.com>
2022-03-07 00:56:26 -0500

    Update 198. House Robber.rs

--- leetcode/198. House Robber.rs
+++ leetcode/198. House Robber.rs
@@ -5,17 +5,16 @@ impl Solution {
         let size = nums.len();  
         if size == 1 {
             return nums[0];
+        } else if size == 2 {
+            return cmp::max(nums[0], nums[1])
         }
         let mut dp = vec![0; size];
         dp[0] = nums[0];
         dp[1] = nums[1];
+        dp[2] = nums[2] + nums[0];
         
-        for i in (2..nums.len()) {
-            if i == 2 {
-                dp[i] = nums[i] + dp[0];
-            } else {
-                dp[i] = nums[i] + cmp::max(dp[i-2], dp[i-3]);
-            }
+        for i in (3..nums.len()) {
+            dp[i] = nums[i] + cmp::max(dp[i-2], dp[i-3]);
         }
         
         return cmp::max(dp[size-1], dp[size-2]);
