523575f7247ed5ddf48d6cd57052ff5d95120332
Yao Cui <cuiy0006@gmail.com>
2022-03-07 01:02:28 -0500

    Update 198. House Robber.py

--- leetcode/198. House Robber.py
+++ leetcode/198. House Robber.py
@@ -13,3 +13,19 @@ class Solution:
                 dp[i] = nums[i] + max(dp[i-2], dp[i-3])
         
         return max(dp[-1], dp[-2])
+
+    
+    
+    
+    
+    
+ class Solution:
+    def rob(self, nums: List[int]) -> int:
+        dp = [[0, 0] for i in range(len(nums))]
+        dp[0] = [0, nums[0]]
+        
+        for i in range(1, len(nums)):
+            dp[i][0] = max(dp[i-1][0], dp[i-1][1])
+            dp[i][1] = nums[i] + dp[i-1][0]
+        
+        return max(dp[-1][0], dp[-1][1])
