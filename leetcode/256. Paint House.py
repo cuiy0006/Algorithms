41a03aaae09597b836b4667430a45f1c78619905
Yao Cui <cuiy0006@gmail.com>
2022-03-12 21:33:07 -0500

    Update 256. Paint House.py

--- leetcode/256. Paint House.py
+++ leetcode/256. Paint House.py
@@ -1,14 +1,12 @@
 class Solution:
     def minCost(self, costs: List[List[int]]) -> int:
-        if len(costs) == 0:
-            return 0
-        dp = [[0, 0, 0] for cost in costs]
+        dp = [[0, 0, 0] for _ in range(len(costs))]
+        
         dp[0] = costs[0][:]
         
         for i in range(1, len(costs)):
-            cost = costs[i]
-            dp[i][0] = cost[0] + min(dp[i-1][1], dp[i-1][2])
-            dp[i][1] = cost[1] + min(dp[i-1][0], dp[i-1][2])
-            dp[i][2] = cost[2] + min(dp[i-1][0], dp[i-1][1])
-            
+            dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + costs[i][0]
+            dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + costs[i][1]
+            dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + costs[i][2]
+        
         return min(dp[-1])
