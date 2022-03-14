c9da47504a064a4fa75534c37db9f48fb045b734
Yao Cui <cuiy0006@gmail.com>
2022-03-13 22:07:51 -0400

    Update 300. Longest Increasing Subsequence.py

--- leetcode/300. Longest Increasing Subsequence.py
+++ leetcode/300. Longest Increasing Subsequence.py
@@ -21,3 +21,16 @@ class Solution(object):
                     left = mid + 1
             lst[left] = nums[i]
         return len(lst)
+
+    
+    
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        dp = [1 for _ in nums]
+        
+        for i in range(1, len(nums)):
+            for j in range(0, i):
+                if nums[i] > nums[j]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+            
+        return max(dp)
