b380417bb11b2f0967ee6b69b188aa1652915ff0
Yao Cui <cuiy0006@gmail.com>
2022-03-13 22:29:31 -0400

    Update 300. Longest Increasing Subsequence.rs

--- leetcode/300. Longest Increasing Subsequence.rs
+++ leetcode/300. Longest Increasing Subsequence.rs
@@ -15,3 +15,34 @@ impl Solution {
         dp.into_iter().max().unwrap()
     }
 }
+
+
+
+use std::cmp;
+
+impl Solution {
+    pub fn length_of_lis(nums: Vec<i32>) -> i32 {
+        let mut v = Vec::new();
+        
+        for num in nums {
+            if v.len() == 0 || v[v.len()-1] < num {
+                v.push(num);
+            } else {
+                let mut i = 0 as usize;
+                let mut j = v.len();
+                while i < j {
+                    let mid: usize = (i + j) / 2;
+                    if num > v[mid] {
+                        i = mid + 1;
+                    } else {
+                        j = mid;
+                    }
+                }
+                
+                v[i] = num;
+            }
+        }
+        
+        v.len() as i32
+    }
+}
