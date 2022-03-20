9a1c625eb7c91edeca3add8df943751093f160c9
Yao Cui <cuiy0006@gmail.com>
2022-03-19 23:21:20 -0400

    Update 53. Maximum Subarray.py

--- leetcode/53. Maximum Subarray.py
+++ leetcode/53. Maximum Subarray.py
@@ -12,3 +12,19 @@ class Solution:
             if curr < 0:
                 curr = 0
         return maxVal
+
+    
+    
+import sys
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        res = -sys.maxsize
+        min_val = max_val = 0
+        for num in nums:
+            max_n = max_val + num
+            min_n = min_val + num
+            max_val = max(num, max_n, min_n)
+            min_val = min(num, max_n, min_n)
+            res = max(res, max_val)
+        return res
