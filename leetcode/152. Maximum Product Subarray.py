600bf259cefbe1d1bee1795ab72cebf5ebce2d14
Yao Cui <cuiy0006@gmail.com>
2022-03-19 23:03:13 -0400

    Update 152. Maximum Product Subarray.py

--- leetcode/152. Maximum Product Subarray.py
+++ leetcode/152. Maximum Product Subarray.py
@@ -13,3 +13,18 @@ class Solution:
             maxVal, minVal = max(num, num * minVal, num * maxVal), min(num, num * minVal, num * maxVal)
             res = max(res, maxVal)
         return res
+
+    
+import sys
+
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        dp = [[1 for _ in range(len(nums)+1)] for _ in range(len(nums)+1)]
+        
+        max_val = -sys.maxsize
+        for i in range(len(nums)):
+            for j in range(i+1):
+                dp[j+1][i+1] = nums[i] * dp[j+1][i]
+                max_val = max(max_val, dp[j+1][i+1])
+                
+        return max_val
