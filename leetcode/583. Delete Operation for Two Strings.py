5a9af29e602f9bb9bab2c2415c8fa2b5cab548f3
Yao Cui <cuiy0006@gmail.com>
2022-03-24 01:19:57 -0400

    Update 583. Delete Operation for Two Strings.py

--- leetcode/583. Delete Operation for Two Strings.py
+++ leetcode/583. Delete Operation for Two Strings.py
@@ -1,19 +1,16 @@
-class Solution(object):
-    def minDistance(self, word1, word2):
-        """
-        :type word1: str
-        :type word2: str
-        :rtype: int
-        """
-        dp = [[0 for w2 in range(len(word2) + 1)] for i in range(len(word1) + 1)]
-        for i in range(len(word1) + 1):
-            for j in range(len(word2) + 1):
-                if i == 0 or j == 0:
-                    continue
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        dp = [[0 for _ in range(len(word2)+1)] for _ in range(len(word1)+1)]
+        for i in range(len(word1)):
+            dp[i+1][0] = i+1
+        for j in range(len(word2)):
+            dp[0][j+1] = j+1
+        
+        for i in range(len(word1)):
+            for j in range(len(word2)):
+                if word1[i] == word2[j]:
+                    dp[i+1][j+1] = dp[i][j]
                 else:
-                    if word1[i - 1] == word2[j - 1]:
-                        dp[i][j] = dp[i - 1][j - 1] + 1
-                    else:
-                        dp[i][j] = max(dp[i][j - 1], dp[i - 1][j])
-                        
-        return len(word1) + len(word2) - 2*dp[-1][-1]
+                    dp[i+1][j+1] = min(dp[i][j+1], dp[i+1][j]) + 1
+        
+        return dp[-1][-1]
