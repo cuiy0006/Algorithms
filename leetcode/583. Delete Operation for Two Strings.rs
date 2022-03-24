9956a21751919d9ab6a7a4ba40b8a283ec1cc9b2
Yao Cui <cuiy0006@gmail.com>
2022-03-24 01:31:37 -0400

    Create 583. Delete Operation for Two Strings.rs

---
+++ leetcode/583. Delete Operation for Two Strings.rs
@@ -0,0 +1,28 @@
+use std::cmp;
+
+impl Solution {
+    pub fn min_distance(word1: String, word2: String) -> i32 {
+        let mut dp = vec![vec![0; word2.len()+1]; word1.len()+1];
+        
+        for i in (0..word1.len()) {
+            dp[i+1][0] = i + 1;
+        }
+        
+        for j in (0..word2.len()) {
+            dp[0][j+1] = j + 1;
+        }
+        
+
+        for (i, c1) in word1.chars().enumerate() {
+            for (j, c2) in word2.chars().enumerate() {
+                if c1 == c2 {
+                    dp[i+1][j+1] = dp[i][j];
+                } else {
+                    dp[i+1][j+1] = cmp::min(dp[i+1][j], dp[i][j+1]) + 1
+                }
+            }
+        }
+        
+        dp[word1.len()][word2.len()] as i32
+    }
+}
