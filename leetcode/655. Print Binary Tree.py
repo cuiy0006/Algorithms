1d96d6156449bb47a264f73da1512362b9842e9b
Yao Cui <cuiy0006@gmail.com>
2022-03-30 23:52:20 -0400

    Update 655. Print Binary Tree.py

--- leetcode/655. Print Binary Tree.py
+++ leetcode/655. Print Binary Tree.py
@@ -33,3 +33,42 @@ class Solution:
         lst = [["" for j in range(width)] for i in range(height)]
         helper(0, width -1, 0, lst, root)
         return lst
+
+    
+    
+    
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+
+class Solution:
+    def printTree(self, root: Optional[TreeNode]) -> List[List[str]]:
+        def get_height(node):
+            if node is None:
+                return 0
+            
+            return max(get_height(node.left), get_height(node.right)) + 1
+        
+        height = get_height(root)
+        
+        width = 2 ** height - 1
+        res = [["" for _ in range(width)] for _ in range(height)]
+        
+        q = deque([(root, 0, (width - 1) // 2)])
+        while len(q) != 0:
+            size = len(q)
+            while size != 0:
+                node, x, y = q.popleft()
+                res[x][y] = str(node.val)
+                if node.left is not None:
+                    q.append((node.left, x+1, y-2**(height-x-2)))
+                if node.right is not None:
+                    q.append((node.right, x+1, y+2**(height-x-2)))
+                size -= 1
+        
+        return res
+        
