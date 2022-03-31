8a0f537b4b54ce4b9716a94c8f0914c50f5c9859
Yao Cui <cuiy0006@gmail.com>
2022-03-31 00:14:58 -0400

    Update 200. Number of Islands.py

--- leetcode/200. Number of Islands.py
+++ leetcode/200. Number of Islands.py
@@ -55,3 +55,30 @@ class Solution(object):
                             visited[x0][y0] = True
                             q.append((x0, y0))
         return cnt
+
+    
+    
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        cnt = 0
+        def find_island(i, j):
+            if i < 0 or i > len(grid) - 1 or j < 0 or j > len(grid[0]) - 1:
+                return
+            
+            if grid[i][j] == '0':
+                return
+            
+            grid[i][j] = '0'
+            find_island(i+1, j)
+            find_island(i-1, j)
+            find_island(i, j+1)
+            find_island(i, j-1)
+            
+        
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    cnt += 1
+                    find_island(i, j)
+        
+        return cnt
