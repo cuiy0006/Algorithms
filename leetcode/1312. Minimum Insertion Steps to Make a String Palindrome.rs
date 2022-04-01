6373e13d608c5c039b136e00e73eea35745e448f
Yao Cui <cuiy0006@gmail.com>
2022-03-31 20:43:18 -0400

    Update 1312. Minimum Insertion Steps to Make a String Palindrome.rs

--- leetcode/1312. Minimum Insertion Steps to Make a String Palindrome.rs
+++ leetcode/1312. Minimum Insertion Steps to Make a String Palindrome.rs
@@ -2,29 +2,26 @@ use std::cmp;
 
 impl Solution {
     pub fn min_insertions(s: String) -> i32 {
-        let word1 = s;
-        let word2 = word1.chars().rev().collect::<String>();
+        let v1: Vec<char> = s.chars().collect();
+        let v2: Vec<char> = s.chars().rev().collect();
         
-        let mut dp = vec![vec![0; word2.len()+1]; word2.len()+1];
+        let mut dp = vec![vec![0; s.len()+1]; s.len()+1];
         
-        for i in (0..word1.len()) {
-            dp[i+1][0] = i + 1;
+        for i in (1..s.len()+1) {
+            dp[i][0] = i;
+            dp[0][i] = i;
         }
         
-        for j in (0..word2.len()) {
-            dp[0][j+1] = j + 1;
-        }
-        
-        for (i, c1) in word1.chars().enumerate() {
-            for (j, c2) in word2.chars().enumerate() {
-                if c1 == c2 {
-                    dp[i+1][j+1] = dp[i][j];
+        for i in (1..v1.len()+1) {
+            for j in (1..v2.len()+1) {
+                if v1[i-1] == v2[j-1] {
+                    dp[i][j] = dp[i-1][j-1]
                 } else {
-                    dp[i+1][j+1] = cmp::min(dp[i+1][j], dp[i][j+1]) + 1
+                    dp[i][j] = cmp::min(dp[i-1][j], dp[i][j-1]) + 1
                 }
             }
         }
         
-        (dp[word1.len()][word2.len()] / 2) as i32
+        (dp[s.len()][s.len()] / 2) as i32
     }
 }
