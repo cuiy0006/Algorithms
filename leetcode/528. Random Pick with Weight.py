aa9aaebafd5ca05b593396af47fd640fa479bfe2
Yao Cui <cuiy0006@gmail.com>
2022-04-03 18:10:49 -0400

    Update 528. Random Pick with Weight.py

--- leetcode/528. Random Pick with Weight.py
+++ leetcode/528. Random Pick with Weight.py
@@ -1,35 +1,31 @@
-import random
+from random import randint
 
 class Solution:
 
     def __init__(self, w: List[int]):
-        self.ranges = []
-        curr = 0
-        for weight in w:
-            curr += weight
-            self.ranges.append(curr)
-
-        self.total = curr
+        self.acc_w = []
+        self.sum_w = 0
         
+        for weight in w:
+            self.sum_w += weight
+            self.acc_w.append(self.sum_w)
 
     def pickIndex(self) -> int:
-        target = random.randint(1, self.total)
-        
+        target = randint(1, self.sum_w)
         i = 0
-        j = len(self.ranges)
+        j = len(self.acc_w)
+        
         while i < j:
             mid = (i + j) // 2
-            if self.ranges[mid] == target:
-                break
-            elif self.ranges[mid] > target:
+            if self.acc_w[mid] == target:
+                return mid
+            elif self.acc_w[mid] > target:
                 j = mid
             else:
                 i = mid + 1
+        return i
+        
 
-        if self.ranges[mid] < target:
-            return mid + 1
-        else:
-            return mid
 
 # Your Solution object will be instantiated and called as such:
 # obj = Solution(w)
