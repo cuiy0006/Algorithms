1279044a1147f3aecc47a300deafed94f693e9a8
Yao Cui <cuiy0006@gmail.com>
2022-04-03 21:54:23 -0400

    Update 50. Pow(x, n).py

--- leetcode/50. Pow(x, n).py
+++ leetcode/50. Pow(x, n).py
@@ -1,19 +1,38 @@
+import sys
+
 class Solution:
-    def myPow(self, x, n):
-        """
-        :type x: float
-        :type n: int
-        :rtype: float
-        """
-        def pow(x, n):
+    def myPow(self, x: float, n: int) -> float:
+        if n == 0:
+            return 1
+        
+        n_positive = n > 0
+        
+        n = abs(n)
+        
+        x_positive = x > 0
+        
+        x = abs(x)
+        
+        def helper(n):
             if n == 0:
                 return 1
-            tmp = pow(x, n//2)
-            if n % 2 == 0:
-                return tmp * tmp
+            is_odd = n % 2
+            sub = helper(n // 2)
+            if sub < 0.000000001:
+                return 0
+            if sys.maxsize / sub < sub:
+                return sys.maxsize
+            
+            if is_odd:
+                return sub * sub * x
             else:
-                return x * tmp * tmp
-        if n >= 0:
-            return pow(x, n)
+                return sub * sub
+        
+        res = helper(n)
+        
+        res = res if n_positive else 1 / res
+        
+        if x_positive:
+            return res
         else:
-            return 1/pow(x, -n)
+            return res if n % 2 == 0 else -res
