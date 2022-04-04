a0e624928586b1aa284b4cd38a6322d153498db1
Yao Cui <cuiy0006@gmail.com>
2022-04-03 21:23:54 -0400

    Update 39. Combination Sum.py

--- leetcode/39. Combination Sum.py
+++ leetcode/39. Combination Sum.py
@@ -1,29 +1,22 @@
 class Solution:
-    def combinationSum(self, candidates, target):
-        """
-        :type candidates: List[int]
-        :type target: int
-        :rtype: List[List[int]]
-        """
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
         
-        def helper(curr_lst, curr_sum, index):
+        def find_comb(idx, curr_sum, curr_lst):
             if curr_sum == target:
                 res.append(curr_lst[:])
                 return
-            elif curr_sum > target:
+            
+            if idx > len(candidates) - 1:
                 return
             
-            for i in range(index, len(candidates)):
-                num = candidates[i]
-                tmp = curr_sum
-                while tmp <= target:
-                    tmp += num
-                    curr_lst.append(num)
-                    helper(curr_lst, tmp, i+1)
-                while tmp != curr_sum:
-                    tmp -= num
-                    curr_lst.pop()
+            while curr_sum <= target:
+                find_comb(idx+1, curr_sum, curr_lst)
+                curr_sum += candidates[idx]
+                curr_lst.append(candidates[idx])
             
-        helper([], 0, 0)
+            while len(curr_lst) > 0 and curr_lst[-1] == candidates[idx]:
+                curr_lst.pop()
+                
+        find_comb(0, 0, [])
         return res
