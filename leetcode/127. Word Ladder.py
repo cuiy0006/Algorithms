9e93442d08a5b53290a30441549a94e63bc24736
Yao Cui <cuiy0006@gmail.com>
2022-04-03 21:03:56 -0400

    Update 127. Word Ladder.py

--- leetcode/127. Word Ladder.py
+++ leetcode/127. Word Ladder.py
@@ -27,3 +27,39 @@ class Solution(object):
                         dic[new_word] = dic[word] + 1
                         q.append(new_word)
         return 0
+
+    
+    
+    
+    
+from collections import deque
+
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        word_set = set(wordList)
+        word_to_depth = {}
+        
+        q = deque([beginWord])
+        
+        depth = 1
+        while len(q) != 0:
+            size = len(q)
+            while size != 0:
+                size -= 1
+                word = q.popleft()
+                if word == endWord:
+                    return depth
+                
+                if word in word_to_depth:
+                    continue
+                
+                word_to_depth[word] = depth
+                if word in word_set:
+                    word_set.remove(word)
+                
+                for other in word_set:
+                    for i in range(len(word)):
+                        if word[:i] == other[:i] and word[i+1:] == other[i+1:]:
+                            q.append(other)
+            depth += 1
+        return 0
