48b0528ad99a4072ebd7332f39cd9f3b92ba7b12
Yao Cui <cuiy0006@gmail.com>
2022-04-05 22:44:47 -0400

    Update 88. Merge Sorted Array.py

--- leetcode/88. Merge Sorted Array.py
+++ leetcode/88. Merge Sorted Array.py
@@ -1,11 +1,7 @@
 class Solution:
-    def merge(self, nums1, m, nums2, n):
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
-        :type nums1: List[int]
-        :type m: int
-        :type nums2: List[int]
-        :type n: int
-        :rtype: void Do not return anything, modify nums1 in-place instead.
+        Do not return anything, modify nums1 in-place instead.
         """
         i = m - 1
         j = len(nums1) - 1
@@ -13,25 +9,30 @@ class Solution:
             nums1[j] = nums1[i]
             i -= 1
             j -= 1
+            
         i = j + 1
         j = 0
-        k = 0
-        while i < len(nums1) and j < n:
-            if nums1[i] < nums2[j]:
-                nums1[k] = nums1[i]
+        idx = 0
+        
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] <= nums2[j]:
+                nums1[idx] = nums1[i]
                 i += 1
             else:
-                nums1[k] = nums2[j]
+                nums1[idx] = nums2[j]
                 j += 1
-            k += 1
+            idx += 1
+        
         while i < len(nums1):
-            nums1[k] = nums1[i]
+            nums1[idx] = nums1[i]
+            idx += 1
             i += 1
-            k += 1
-        while j < n:
-            nums1[k] = nums2[j]
+            
+        while j < len(nums2):
+            nums1[idx] = nums2[j]
+            idx += 1
             j += 1
-            k += 1
+            
             
             
             
