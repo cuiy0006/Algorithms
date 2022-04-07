fc1417b3d002009f40f90bdc22cb6bff045ba9b2
Yao Cui <cuiy0006@gmail.com>
2022-04-06 22:48:51 -0400

    Update 18. 4Sum.py

--- leetcode/18. 4Sum.py
+++ leetcode/18. 4Sum.py
@@ -1,32 +1,33 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        nums.sort()
         res = []
+        nums.sort()
+        i = 0
         
-        for i in range(len(nums)):
-            if i != 0 and nums[i] == nums[i - 1]:
-                continue
-            
-            sub_target = target - nums[i]
-            for j in range(i + 1, len(nums)):
-                if j != i + 1 and nums[j] == nums[j - 1]:
-                    continue
-                x = j + 1
-                y = len(nums) - 1
-                sub_sub_target = sub_target - nums[j]
-                while x < y:
-                    total = nums[x] + nums[y]
-                    if total == sub_sub_target:
-                        res.append([nums[i], nums[j], nums[x], nums[y]])
-                        x += 1
-                        y -= 1
-                        while x < y and nums[x] == nums[x - 1]:
-                            x += 1
-                        while x < y and nums[y] == nums[y + 1]:
-                            y -= 1
-                            
-                    elif total > sub_sub_target:
-                        y -= 1
+        while i < len(nums):
+            j = len(nums) - 1
+            while j > i:
+                left = target - nums[i] - nums[j]
+                m = i + 1
+                n = j - 1
+                while m < n:
+                    total = nums[m] + nums[n]
+                    if total == left:
+                        res.append([nums[i], nums[m], nums[n], nums[j]])
+                        m += 1
+                        while m < n and nums[m] == nums[m-1]:
+                            m += 1
+                    elif total > left:
+                        n -= 1
                     else:
-                        x += 1
+                        m += 1
+                j -= 1
+                while j > i and nums[j] == nums[j+1]:
+                    j -= 1
+            
+            i += 1
+            while i < len(nums) and nums[i] == nums[i-1]:
+                i += 1
+        
         return res
+                    
