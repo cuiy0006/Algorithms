6d5d8f80026a9fe1f97542230263d5333919f13f
Yao Cui <cuiy0006@gmail.com>
2022-04-06 21:26:51 -0400

    Update 23. Merge k Sorted Lists.py

--- leetcode/23. Merge k Sorted Lists.py
+++ leetcode/23. Merge k Sorted Lists.py
@@ -31,5 +31,49 @@ class Solution:
                 tail.next = second
             q.append(head.next)
         return q.popleft()
-                
+             
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+from collections import deque
+
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        def merge_two(root1, root2):
+            node = root = ListNode()
+            node1 = root1
+            node2 = root2
             
+            while node1 and node2:
+                if node1.val < node2.val:
+                    node.next = node1
+                    node1 = node1.next
+                else:
+                    node.next = node2
+                    node2 = node2.next
+                    
+                node = node.next
+            
+            while node1:
+                node.next = node1
+                node = node.next
+                node1 = node1.next
+            
+            while node2:
+                node.next = node2
+                node = node.next
+                node2 = node2.next
+            return root.next
+        
+        q = deque(lists)
+        
+        while len(q) > 1:
+            node1 = q.popleft()
+            node2 = q.popleft()
+            
+            q.append(merge_two(node1, node2))
+        
+        return q.popleft() if len(q) > 0 else None
