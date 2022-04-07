2c1cd95b6323b32bd9558cef5b674e3e81ba7769
Yao Cui <cuiy0006@gmail.com>
2022-04-06 22:15:38 -0400

    Update 994. Rotting Oranges.py

--- leetcode/994. Rotting Oranges.py
+++ leetcode/994. Rotting Oranges.py
@@ -2,38 +2,41 @@ from collections import deque
 
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        minute = 0
-        total = 0
-        
         q = deque()
+        dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        
+        fresh = 0
         for i in range(len(grid)):
-            for j in range(len(grid[i])):
+            for j in range(len(grid[0])):
                 if grid[i][j] == 2:
                     q.append((i, j))
                 if grid[i][j] == 1:
-                    total += 1
+                    fresh += 1
         
-        dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-        
+        turn = 0
         while len(q) != 0:
             size = len(q)
-            for i in range(size):
+            while size > 0:
+                size -= 1
                 x, y = q.popleft()
-                for d in dirs:
-                    x0, y0 = x + d[0], y + d[1]
-                    if x0 < 0 or y0 < 0 or x0 > len(grid) - 1 or y0 > len(grid[0]) - 1:
+                
+                for (x0, y0) in dirs:
+                    x1 = x + x0
+                    y1 = y + y0
+                    
+                    if x1 < 0 or x1 > len(grid) - 1 or y1 < 0 or y1 > len(grid[0]) - 1:
                         continue
                     
-                    if grid[x0][y0] == 1:
-                        grid[x0][y0] = 2
-                        total -= 1
-                        q.append((x0, y0))
-            if len(q) == 0:
-                break
-            
-            minute += 1
+                    if grid[x1][y1] != 1:
+                        continue
+                    
+                    fresh -= 1
+                    grid[x1][y1] = 2
+                    q.append((x1, y1))
+            if len(q) != 0:
+                turn += 1
         
-        if total != 0:
+        if fresh > 0:
             return -1
-        else:
-            return minute
+        
+        return turn
