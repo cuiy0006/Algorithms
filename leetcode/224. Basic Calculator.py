d50613831db76234ad24060bb566af3de7248901
Yao Cui <cuiy0006@gmail.com>
2022-04-13 01:44:07 -0400

    Update 224. Basic Calculator.py

--- leetcode/224. Basic Calculator.py
+++ leetcode/224. Basic Calculator.py
@@ -23,3 +23,58 @@ class Solution:
                 sign = stack.pop()
 
         return res + sign * num
+
+    
+    
+    
+    
+    
+class Solution:
+    def calculate(self, s: str) -> int:
+        def evaluate(s) -> int:
+            s0 = s
+            s = ''
+            for c in s0:
+                if c != ' ':
+                    s += c
+
+            res = 0
+            i = 0
+            s = '+' + s
+            while i < len(s):
+                if s[i] == ' ':
+                    i += 1
+                    continue
+                if s[i] == '+' or s[i] == '-':
+                    j = i + 1
+                    if s[j] == '+' or s[j] == '-':
+                        j += 1
+                    
+                    while j < len(s) and s[j] != '+' and s[j] != '-':
+                        j += 1
+
+                    num = int(s[i+1:j])
+                    if s[i] == '+':
+                        res += num
+                    else:
+                        res -= num
+                    i = j
+                else:
+                    i += 1
+            return res
+        
+        stack = []
+        i = 0
+        curr = ''
+        while i < len(s):
+            if s[i] == '(':
+                stack.append(curr)
+                curr = ''
+            elif s[i] == ')':
+                num = evaluate(curr)
+                curr = stack.pop() + str(num)
+            else:
+                curr += s[i]
+            i += 1
+        
+        return evaluate(curr)
