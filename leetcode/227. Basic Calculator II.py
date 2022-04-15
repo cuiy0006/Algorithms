34a2a903af3a1e6855cdb2397caff12eb0553682
Yao Cui <cuiy0006@gmail.com>
2022-04-15 00:57:30 -0400

    Update 227. Basic Calculator II.py

--- leetcode/227. Basic Calculator II.py
+++ leetcode/227. Basic Calculator II.py
@@ -29,3 +29,46 @@ class Solution:
                 curr = ''
                 sign = c
         return sum(stack)
+
+    
+ class Solution:
+    def calculate(self, s: str) -> int:
+        s0 = s
+        s = ''
+        for c in s0:
+            if c != ' ':
+                s += c
+        
+        stack = []
+        curr = ''
+        i = 0
+        while i < len(s):
+            c = s[i]
+            if c.isdigit():
+                curr += c
+                i += 1
+            elif c == '+' or c == '-':
+                stack.append(int(curr))
+                curr = ''
+                curr += c
+                i += 1
+            else:
+                stack.append(int(curr))
+                curr = ''
+                j = i + 1
+                while j < len(s):
+                    if s[j].isdigit():
+                        j += 1
+                    else:
+                        break
+                second = int(s[i+1:j])
+                if c == '*':
+                    curr = stack.pop() * second
+                else:
+                    curr = int(stack.pop() / second)
+                i = j
+
+        if curr != '':
+            stack.append(int(curr))
+            
+        return sum(stack)
