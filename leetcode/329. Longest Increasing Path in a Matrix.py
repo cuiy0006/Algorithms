103c8f328ae042f14a45f96e1f6d1d57891f93e1
Yao Cui <cuiy0006@gmail.com>
2022-04-18 17:20:42 -0400

    Update 329. Longest Increasing Path in a Matrix.py

--- leetcode/329. Longest Increasing Path in a Matrix.py
+++ leetcode/329. Longest Increasing Path in a Matrix.py
@@ -1,27 +1,26 @@
-import sys
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
         m = len(matrix)
-        if m == 0:
-            return 0
         n = len(matrix[0])
-        distances = [[-1 for i in range(n)] for j in range(m)]
-        def helper(x, y, last):
-            if x < 0 or y < 0 or x >= m or y >= n or last >= matrix[x][y]:
+        
+        dp = [[-1 for _ in range(n)] for _ in range(m)]
+        directions = ((1, 0), (-1, 0), (0, 1), (0, -1))
+        
+        def get_path(x, y, last):
+            if x < 0 or x > m - 1 or y < 0 or y > n - 1:
                 return 0
-            if distances[x][y] != -1:
-                return distances[x][y]
+            if last is not None and last >= matrix[x][y]:
+                return 0
             
-            left = helper(x - 1, y, matrix[x][y])
-            right = helper(x + 1, y, matrix[x][y])
-            up = helper(x, y - 1, matrix[x][y])
-            down = helper(x, y + 1, matrix[x][y])
-            distances[x][y] = 1 + max(left, right, up, down)
-            return distances[x][y]
+            if dp[x][y] != -1:
+                return dp[x][y]
+            
+            for (x0, y0) in directions:
+                dp[x][y] = max(dp[x][y], get_path(x+x0, y+y0, matrix[x][y]) + 1)
+            return dp[x][y]
         
         res = 0
         for i in range(m):
             for j in range(n):
-                tmp = helper(i, j, -sys.maxsize)
-                res = max(res, tmp)
+                res = max(res, get_path(i, j, None))
         return res
