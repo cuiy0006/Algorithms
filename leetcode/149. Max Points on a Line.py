871f335c8e596ba3a11e3332a9b59c152e8c9d15
Yao Cui <cuiy0006@gmail.com>
2022-04-18 00:22:29 -0400

    Update 149. Max Points on a Line.py

--- leetcode/149. Max Points on a Line.py
+++ leetcode/149. Max Points on a Line.py
@@ -45,48 +45,27 @@ class Solution:
 
 
 
-# Definition for a point.
-# class Point:
-#     def __init__(self, a=0, b=0):
-#         self.x = a
-#         self.y = b
 
 class Solution:
-    def maxPoints(self, points):
-        """
-        :type points: List[Point]
-        :rtype: int
-        """
-        if len(points) == 0:
-            return 0
+    def maxPoints(self, points: List[List[int]]) -> int:
         if len(points) == 1:
             return 1
-        dic = {}
-        for point in points:
-            point = (point.x, point.y)
-            if point in dic:
-                dic[point] += 1
-            else:
-                dic[point] = 1
-        lst = list(dic.keys())
-        kdic = {}
-        if len(lst) == 1:
-            return dic[lst[0]]
+        res = 0
         
-        for i, first in enumerate(lst):
-            j = i + 1
-            while j < len(lst):
-                second = lst[j]
-                key = None
-                if second[0] - first[0] == 0:
-                    key = ('y', i)
+        for i in range(len(points)):
+            [x1, y1] = points[i]
+            kp_to_cnt = {}
+            for j in range(i+1, len(points)):
+                [x2, y2] = points[j]
+                
+                if x1 == x2:
+                    k = 'inf'
                 else:
-                    k = (second[1] - first[1])/(second[0] - first[0])
-                    key = (k, i)
-                if key in kdic:
-                    kdic[key] += dic[second]
-                else:
-                    kdic[key] = dic[first] + dic[second]
-                j += 1
-        return max(kdic.values())
+                    k = (y1 - y2) / (x1 - x2)
+                if (k, i) not in kp_to_cnt:
+                    kp_to_cnt[(k, i)] = 1
+                kp_to_cnt[(k, i)] += 1
+                res = max(res, kp_to_cnt[(k, i)])
+        
+        return res
                     
