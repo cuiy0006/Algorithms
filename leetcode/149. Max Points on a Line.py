00ffd808fd3983b5045bd21efac77298fae024b4
Yao Cui <cuiy0006@gmail.com>
2022-04-18 00:07:05 -0400

    Update 149. Max Points on a Line.py

--- leetcode/149. Max Points on a Line.py
+++ leetcode/149. Max Points on a Line.py
@@ -1,3 +1,50 @@
+class Solution:
+    def maxPoints(self, points: List[List[int]]) -> int:
+        if len(points) == 1:
+            return 1
+        k_to_lines = {}
+        
+        for i in range(len(points)):
+            for j in range(i+1, len(points)):
+                p1 = points[i]
+                p2 = points[j]
+                
+                if p1[0] == p2[0]:
+                    k = 'inf'
+                else:
+                    k = (p1[1] - p2[1]) / (p1[0] - p2[0])
+                
+                if k not in k_to_lines:
+                    k_to_lines[k] = []
+                k_to_lines[k].append((tuple(p1), tuple(p2)))
+        
+        def find_ancestor(p, dic):
+            while p in dic and dic[p] != p:
+                p = dic[p]
+            return p
+        
+        res = 0
+        for lst in k_to_lines.values():
+            dic = {}
+            for (p1, p2) in lst:
+                ancestor1 = find_ancestor(p1, dic)
+                ancestor2 = find_ancestor(p2, dic)
+                dic[ancestor1] = ancestor1
+                dic[p1] = ancestor1
+                dic[ancestor2] = ancestor1
+                dic[p2] = ancestor1
+            
+            ancestor_to_p = {}
+            for p in dic:
+                ancestor = find_ancestor(p, dic)
+                if ancestor not in ancestor_to_p:
+                    ancestor_to_p[ancestor] = []
+                ancestor_to_p[ancestor].append(p)
+                res = max(res, len(ancestor_to_p[ancestor]))
+        return res
+
+
+
 # Definition for a point.
 # class Point:
 #     def __init__(self, a=0, b=0):
