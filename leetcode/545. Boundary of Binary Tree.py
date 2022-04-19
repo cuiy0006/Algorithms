55e894c68137c0fc6753f7dc6c155682267df42f
Yao Cui <cuiy0006@gmail.com>
2022-04-19 02:30:18 -0400

    Update 545. Boundary of Binary Tree.py

--- leetcode/545. Boundary of Binary Tree.py
+++ leetcode/545. Boundary of Binary Tree.py
@@ -1,56 +1,66 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 from collections import deque
-class Solution(object):
-    def boundaryOfBinaryTree(self, root):
-        """
-        :type root: TreeNode
-        :rtype: List[int]
-        """
-        if root == None:
+
+class Solution:
+    def boundaryOfBinaryTree(self, root: Optional[TreeNode]) -> List[int]:
+        if root is None:
             return []
-        left = []
-        right = []
+
+        def is_leaf(node):
+            if node.left is None and node.right is None:
+                return True
+            else:
+                return False
+        
+        if is_leaf(root):
+            return [root.val]
+        
         leaves = []
+        def find_leaves(node):
+            if node is None:
+                return
+            
+            if is_leaf(node):
+                leaves.append(node.val)
+            find_leaves(node.left)
+            find_leaves(node.right)
         
-        if root.left != None:
-            self.findLeft(root.left, left)
-            self.findLeaves(root.left, leaves)
-        if root.right !=None:
-            self.findLeaves(root.right, leaves)
-            self.findRight(root.right, right)
+        find_leaves(root)
+
+        left = []
+        def find_left_boundary(node):
+            if node is None:
+                return
+            if is_leaf(node):
+                return
+            left.append(node.val)
+            if node.left is not None:
+                find_left_boundary(node.left)
+            else:
+                find_left_boundary(node.right)
+        
+        find_left_boundary(root.left)
+        
+        right = []
+        def find_right_boundary(node):
+            if node is None:
+                return
+            if is_leaf(node):
+                return
+            right.append(node.val)
+            if node.right is not None:
+                find_right_boundary(node.right)
+            else:
+                find_right_boundary(node.left)
+        
+        find_right_boundary(root.right)
         right.reverse()
+        
         return [root.val] + left + leaves + right
-                    
-    def findLeft(self, node, left):
-        if node.left == None and node.right == None:
-            return
-        left.append(node.val)
-        
-        if node.left != None:
-            self.findLeft(node.left, left)
-        else:
-            self.findLeft(node.right, left)
-    
-    def findRight(self, node, right):
-        if node.left == None and node.right == None:
-            return
-        right.append(node.val)
-        
-        if node.right != None:
-            self.findRight(node.right, right)
-        else:
-            self.findRight(node.left, right)
-                
-    def findLeaves(self, node, leaves):
-        if node.left == None and node.right == None:
-            leaves.append(node.val)
-            return
-        if node.left != None:
-            self.findLeaves(node.left, leaves)
-        if node.right != None:
-            self.findLeaves(node.right, leaves)
+
+            
