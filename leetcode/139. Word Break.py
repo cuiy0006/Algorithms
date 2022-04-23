b18ecf55b64cd8ece7133e83eb82b5df6eac2b25
Yao Cui <cuiy0006@gmail.com>
2022-04-23 02:00:27 -0400

    Update 139. Word Break.py

--- leetcode/139. Word Break.py
+++ leetcode/139. Word Break.py
@@ -1,45 +1,17 @@
-class Solution(object):
-    def wordBreak(self, s, wordDict):
-        """
-        :type s: str
-        :type wordDict: List[str]
-        :rtype: bool
-        """
-        if len(wordDict) == 0:
-            return False
-        dic = set(wordDict)
-        dp = [False] * len(s)
-        maxlen = len(max(wordDict, key=lambda x:len(x)))
-        for i in range(len(s)):
-            j = i
-            while j >= 0 and i - j < maxlen:
-                sub = s[j:i+1]
-                if sub in dic and (j == 0 or dp[j-1]):
-                    dp[i] = True
-                    break
-                j -= 1
-        return dp[-1]
-    
-    
-    
-    
-    
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        words = set(wordDict)
-        dp = [False] * (len(s) + 1)
-
+        max_len = max([len(word) for word in wordDict])
+        word_set = set(wordDict)
+        
+        dp = [False] * (len(s)+1)
         dp[0] = True
-
-        for i in range(len(dp)):
-            reached = dp[i]
-            if not reached:
+        
+        for i in range(len(s)):
+            if not dp[i]:
                 continue
-
-            for word in words:
-                if i + len(word) > len(s):
-                    continue
-                if s[i: i + len(word)] == word:
-                    dp[i + len(word)] = True
+            for j in range(i, min(len(s), i+max_len)):
+                word = s[i:j+1]
+                if word in word_set:
+                    dp[j+1] = True
 
         return dp[-1]
