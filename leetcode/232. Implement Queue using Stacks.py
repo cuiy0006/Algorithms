d5e6d6e15fe952aa3c3fcf0f3140fd9e871a2ae8
Yao Cui <cuiy0006@gmail.com>
2022-04-28 00:49:02 -0400

    Update 232. Implement Queue using Stacks.py

--- leetcode/232. Implement Queue using Stacks.py
+++ leetcode/232. Implement Queue using Stacks.py
@@ -1,50 +1,26 @@
 class MyQueue:
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
         self.stack1 = []
         self.stack2 = []
-        
 
-    def push(self, x):
-        """
-        Push element x to the back of queue.
-        :type x: int
-        :rtype: void
-        """
-        self.stack1.append(x)
+    def push(self, x: int) -> None:
+        self.stack1.append(x)        
 
-    def pop(self):
-        """
-        Removes the element from in front of queue and returns that element.
-        :rtype: int
-        """
-        if len(self.stack2) != 0:
-            return self.stack2.pop()
-        while len(self.stack1) > 1:
-            self.stack2.append(self.stack1.pop())
-        return self.stack1.pop()
+    def pop(self) -> int:
+        if len(self.stack2) == 0:
+            while len(self.stack1) != 0:
+                self.stack2.append(self.stack1.pop())
+        return self.stack2.pop()
 
-    def peek(self):
-        """
-        Get the front element.
-        :rtype: int
-        """
-        if len(self.stack2) != 0:
-            return self.stack2[-1]
-        while len(self.stack1) != 0:
-            self.stack2.append(self.stack1.pop())
+    def peek(self) -> int:
+        if len(self.stack2) == 0:
+            while len(self.stack1) != 0:
+                self.stack2.append(self.stack1.pop())
         return self.stack2[-1]
 
-    def empty(self):
-        """
-        Returns whether the queue is empty.
-        :rtype: bool
-        """
+    def empty(self) -> bool:
         return len(self.stack1) == 0 and len(self.stack2) == 0
-        
 
 
 # Your MyQueue object will be instantiated and called as such:
