78854742d12c1d6307f0bd1c117a403715447a2d
Yao Cui <cuiy0006@gmail.com>
2022-04-29 01:10:43 -0400

    Update 314. Binary Tree Vertical Order Traversal.py

--- leetcode/314. Binary Tree Vertical Order Traversal.py
+++ leetcode/314. Binary Tree Vertical Order Traversal.py
@@ -1,31 +1,28 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-from collections import deque
-class Solution(object):
-    def verticalOrder(self, root):
-        """
-        :type root: TreeNode
-        :rtype: List[List[int]]
-        """
-        if root == None:
-            return []
-        
-        dic = {}
-        q = deque([(0, root)])
-        while len(q) != 0:
-            depth, node = q.popleft()
-            if depth in dic:
-                dic[depth].append(node.val)
-            else:
-                dic[depth] = [node.val]
-            
-            if node.left != None:
-                q.append((depth - 1, node.left))
-            if node.right != None:
-                q.append((depth + 1, node.right))
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 
-        return [dic[key] for key in sorted(list(dic.keys()))]
+from collections import deque
+
+class Solution:
+    def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if root is None:
+            return []
+        dic = defaultdict(list)
+        
+        q = deque([(root, 0)])
+        
+        while len(q) != 0:
+            size = len(q)
+            for _ in range(size):
+                node, idx = q.popleft()
+                dic[idx].append(node.val)
+                if node.left is not None:
+                    q.append((node.left, idx-1))
+                if node.right is not None:
+                    q.append((node.right, idx+1))
+        
+        return [dic[key] for key in sorted(dic.keys())]
