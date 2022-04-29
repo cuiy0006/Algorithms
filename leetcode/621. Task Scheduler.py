4f9416de9f61d2df0128b4cabc2076ad1f795e68
Yao Cui <cuiy0006@gmail.com>
2022-04-29 03:15:14 -0400

    Update 621. Task Scheduler.py

--- leetcode/621. Task Scheduler.py
+++ leetcode/621. Task Scheduler.py
@@ -1,52 +1,22 @@
-from heapq import heappush
-from heapq import heappop
-class Solution:
-    def leastInterval(self, tasks, n):
-        """
-        :type tasks: List[str]
-        :type n: int
-        :rtype: int
-        """
-        dic = {}
-        for task in tasks:
-            if task in dic:
-                dic[task] += 1
-            else:
-                dic[task] = 1
-        h = []
-        cnt = 0
-        for v in dic.values():
-            heappush(h, -v)
-        while len(h) != 0:
-            k = n + 1
-            lst = []
-            while k > 0 and len(h) != 0:
-                freq = heappop(h)
-                if freq + 1 < 0:
-                    lst.append(freq+1)
-                cnt += 1
-                k -= 1
-            for freq in lst:
-                heappush(h, freq)
-            if k != 0 and len(h) != 0:
-                cnt += k
-        return cnt
-
-
-
-
-
-
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        dic = {}
+        max_cnt = 0
+        task_to_cnt = defaultdict(int)
+        
         for task in tasks:
-            if task not in dic:
-                dic[task] = 0
-            dic[task] += 1
+            task_to_cnt[task] += 1
+            max_cnt = max(max_cnt, task_to_cnt[task])
         
-        freqs = list(dic.values())
-        max_freq = max(freqs)
-        num_max_freq = freqs.count(max_freq)
+        num_max_cnt = 0
+        for cnt in task_to_cnt.values():
+            if cnt == max_cnt:
+                num_max_cnt += 1
         
-        return max(len(tasks), (n + 1) * (max_freq - 1) + num_max_freq
+        if num_max_cnt >= n+1:
+            return len(tasks)
+        else:
+            res = (n+1)*(max_cnt-1) + num_max_cnt
+            if res < len(tasks):
+                return len(tasks)
+            else:
+                return res
