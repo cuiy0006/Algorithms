b24f21c7b77ce167f2bd0279baa34bc42b9534b7
Yao Cui <cuiy0006@gmail.com>
2022-04-30 01:26:50 -0400

    Update 43. Multiply Strings.py

--- leetcode/43. Multiply Strings.py
+++ leetcode/43. Multiply Strings.py
@@ -1,29 +1,26 @@
-class Solution(object):
-    def multiply(self, num1, num2):
-        """
-        :type num1: str
-        :type num2: str
-        :rtype: str
-        """
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
         if num1 == '0' or num2 == '0':
             return '0'
         m = len(num1)
         n = len(num2)
-        res = [0] * (m + n)
-        for i, c1 in enumerate(num1[::-1]):
-            for j, c2 in enumerate(num2[::-1]):
-                pos = i + j
-                tmp = (ord(c1) - ord('0')) * (ord(c2) - ord('0')) + res[pos]
-                q = tmp // 10
-                r = tmp % 10
-                res[pos] = r
-                res[pos + 1] += q
-        res = res[::-1]
-        for i, val in enumerate(res):
-            if val != 0:
-                res = res[i:]
-                break
-        return ''.join(map(str, res))
+        arr = [0] * (m+n)
+
+        for i, c1 in enumerate(num2[::-1]):
+            for j, c2 in enumerate(num1[::-1]):
+                total = (ord(c1)-ord('0')) * (ord(c2)-ord('0')) + arr[i+j]
+                arr[i+j] = total % 10
+                arr[i+j+1] += total // 10 
+        
+        arr.reverse()
+        
+        i = 0
+        while i < len(arr) and arr[i] == 0:
+            i += 1
+        arr = arr[i:]
+        
+        return ''.join([str(digit) for digit in arr])
+                
         
 
         
