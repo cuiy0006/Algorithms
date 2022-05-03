45fb28d4dcd67fdbdd1bced0f4488e7ffb0c4b88
Yao Cui <cuiy0006@gmail.com>
2022-05-02 22:34:09 -0400

    Update 636. Exclusive Time of Functions.py

--- leetcode/636. Exclusive Time of Functions.py
+++ leetcode/636. Exclusive Time of Functions.py
@@ -1,26 +1,21 @@
 class Solution:
     def exclusiveTime(self, n: int, logs: List[str]) -> List[int]:
-        id_to_duration = {}
         stack = []
-        last_ts = None
+        res = [0] * n
         
         for log in logs:
-            tp = log.split(':')
-            fid = tp[0]
-            state = tp[1]
-            ts = int(tp[2])
+            [pid, state, ts] = log.split(':')
+            pid = int(pid)
+            ts = int(ts)
             
             if state == 'start':
-                if fid not in id_to_duration:
-                    id_to_duration[fid] = 0
-                if len(stack) != 0:
-                    id_to_duration[stack[-1]] += ts - last_ts
-                
-                stack.append(fid)
-                last_ts = ts
+                stack.append([pid, ts, 0])
             else:
-                id_to_duration[stack.pop()] += ts - last_ts + 1
-                last_ts = ts + 1
-
+                [pid, start_ts, used] = stack.pop()
+                res[pid] += ts - start_ts + 1 - used
+                
+                if len(stack) != 0:
+                    stack[-1][2] += ts - start_ts + 1
         
-        return [duration for duration in id_to_duration.values()]
+        return res
+            
