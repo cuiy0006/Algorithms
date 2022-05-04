2ad5f29bc611d5d61c4458d4aed4faf4e0f5e149
Yao Cui <cuiy0006@gmail.com>
2022-05-03 21:30:48 -0400

    Update 2040. Kth Smallest Product of Two Sorted Arrays.py

--- leetcode/2040. Kth Smallest Product of Two Sorted Arrays.py
+++ leetcode/2040. Kth Smallest Product of Two Sorted Arrays.py
@@ -1,48 +1,36 @@
+import bisect
+
 class Solution:
     def kthSmallestProduct(self, nums1: List[int], nums2: List[int], k: int) -> int:
-        def find_count(nums2, num, target):
-            if num == 0:
-                return len(nums2) if target >= 0 else 0
-            
-            target = target / num
-            
-            left = 0
-            right = len(nums2)
-            if num > 0:
-                while left < right:
-                    mid = (left + right) // 2
-                    if nums2[mid] <= target:
-                        left = mid + 1
-                    else:
-                        right = mid
-                return left
-            else:
-                while left < right:
-                    mid = (left + right) // 2
-                    if nums2[mid] >= target:
-                        right = mid
-                    else:
-                        left = mid + 1
-                return len(nums2) - left
-                    
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
+        
+        def count_smaller_or_equal(m):
+            res = 0
+            for i in range(len(nums1)):
+                num1 = nums1[i]
+                if num1 == 0:
+                    if m >= 0:
+                        res += len(nums2)
+                elif num1 > 0:
+                    target = m / num1
+                    idx = bisect.bisect_right(nums2, target)
+                    res += idx
+                else:
+                    target = m / num1
+                    idx = bisect.bisect_left(nums2, target)
+                    res += len(nums2)-idx
+            return res
         
         left = -10000000000
         right = 10000000000
         
-        if len(nums1) > len(nums2):
-            nums1, nums2 = nums2, nums1
-        
-        res = 0
         while left < right:
             mid = (left + right) // 2
-            cnt = 0
-            for num1 in nums1:
-                cnt += find_count(nums2, num1, mid)
-            
-            if cnt >= k:
-                res = mid
-                right = mid
-            else:
+            cnt = count_smaller_or_equal(mid)
+            if cnt < k:
                 left = mid + 1
-        
-        return res
+            else:
+                right = mid
+                
+        return left
