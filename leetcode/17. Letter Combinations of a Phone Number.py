02452890c300740df199aab7b999eab8e4b52fcd
Yao Cui <cuiy0006@gmail.com>
2022-05-04 23:56:48 -0400

    Update 17. Letter Combinations of a Phone Number.py

--- leetcode/17. Letter Combinations of a Phone Number.py
+++ leetcode/17. Letter Combinations of a Phone Number.py
@@ -19,25 +19,28 @@ class Solution:
 
 
 
-dic = {'1':'*', '2':'abc','3':'def','4':'ghi','5':'jkl','6':'mno','7':'pqrs','8':'tuv','9':'wxyz','0':' '}
-class Solution(object):
-    def letterCombinations(self, digits):
-        """
-        :type digits: str
-        :rtype: List[str]
-        """
-        def helper(digit, lst):
-            new_lst = []
-            chars = dic[digit]
-            for word in lst:
-                for c in chars:
-                    new_lst.append(word + c)
-            return new_lst
-        
-        if len(digits) == 0:
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if digits == '':
             return []
+        dic = {'2':'abc',
+                '3': 'def',
+                '4': 'ghi',
+                '5': 'jkl',
+                '6': 'mno',
+                '7': 'pqrs',
+                '8': 'tuv',
+                '9': 'wxyz'}
+        res = []
+        digits = list(digits)
         
-        res = ['']
-        for digit in digits:
-            res = helper(digit, res)
+        def helper(idx, curr):
+            if idx == len(digits):
+                res.append(curr)
+                return
+            
+            for c in dic[digits[idx]]:
+                helper(idx+1, curr+c)
+        
+        helper(0, '')
         return res
