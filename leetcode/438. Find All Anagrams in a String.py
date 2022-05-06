add753811b24b7fe95eafc801c104f49cc64f90d
Yao Cui <cuiy0006@gmail.com>
2022-05-05 23:20:58 -0400

    Update 438. Find All Anagrams in a String.py

--- leetcode/438. Find All Anagrams in a String.py
+++ leetcode/438. Find All Anagrams in a String.py
@@ -1,5 +1,41 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
+        dic = defaultdict(int)
+        
+        for c in p:
+            dic[c] += 1
+        
+        cnt = len(dic)
+        res = []
+        
+        for i, c in enumerate(s):
+            if i < len(p):
+                if c in dic:
+                    dic[c] -= 1
+                    if dic[c] == 0:
+                        cnt -= 1
+                        if cnt == 0:
+                            res.append(0)
+            else:
+                c0 = s[i-len(p)]
+                if c0 in dic:
+                    dic[c0] += 1
+                    if dic[c0] == 1:
+                        cnt += 1
+                if c in dic:
+                    dic[c] -= 1
+                    if dic[c] == 0:
+                        cnt -= 1
+                        if cnt == 0:
+                            res.append(i-len(p)+1)
+        
+        return res
+
+
+
+
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
         dic = {}
         for c in p:
             if c not in dic:
