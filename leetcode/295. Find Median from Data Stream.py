10bf7d9960837ba2fe2234f5fa6c2f0a4218653b
Yao Cui <cuiy0006@gmail.com>
2022-05-05 20:44:07 -0400

    Update 295. Find Median from Data Stream.py

--- leetcode/295. Find Median from Data Stream.py
+++ leetcode/295. Find Median from Data Stream.py
@@ -3,36 +3,24 @@ from heapq import heappush, heappop
 class MedianFinder:
 
     def __init__(self):
-        """
-        initialize your data structure here.
-        """
-        self.left = []
-        self.right = []
+        self.sh = []
+        self.lh = []
+        
 
     def addNum(self, num: int) -> None:
-        if len(self.right) != 0 and num >= self.right[0]:
-            if len(self.right) > len(self.left):
-                heappush(self.left, -heappop(self.right))
-            heappush(self.right, num)
-        elif len(self.left) != 0 and num <= -self.left[0]:
-            if len(self.left) > len(self.right):
-                heappush(self.right, -heappop(self.left))
-            heappush(self.left, -num)
+        if len(self.sh) == len(self.lh):
+            heappush(self.lh, -num)
+            heappush(self.sh, -heappop(self.lh))
         else:
-            if len(self.left) < len(self.right):
-                heappush(self.left, -num)
-            else:
-                heappush(self.right, num)
-            
+            heappush(self.sh, num)
+            heappush(self.lh, -heappop(self.sh))
+        
 
     def findMedian(self) -> float:
-        if len(self.left) > len(self.right):
-            return -self.left[0]
-        elif len(self.left) < len(self.right):
-            return self.right[0]
+        if len(self.sh) == len(self.lh):
+            return (self.sh[0] - self.lh[0]) / 2
         else:
-            return (-self.left[0] + self.right[0]) / 2
-        
+            return self.sh[0]
 
 
 # Your MedianFinder object will be instantiated and called as such:
