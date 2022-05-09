af84ae9ea84edd7d3728a182ec5eadf3157cd1fe
Yao Cui <cuiy0006@gmail.com>
2022-05-09 17:16:14 -0400

    Update 254. Factor Combinations.py

--- leetcode/254. Factor Combinations.py
+++ leetcode/254. Factor Combinations.py
@@ -1,6 +1,26 @@
 class Solution:
     def getFactors(self, n: int) -> List[List[int]]:
         res = []
+        
+        def helper(n, curr, start):
+            if n == 1:
+                return
+            
+            for i in range(start, int(n**0.5)+1):
+                if n % i == 0:
+                    j = n // i
+                    res.append(curr + [i, j])
+                    curr.append(i)
+                    helper(j, curr, i)
+                    curr.pop()
+        
+        helper(n, [], 2)
+        return res
+
+
+class Solution:
+    def getFactors(self, n: int) -> List[List[int]]:
+        res = []
         lst = []
         
         def helper(n, lower):
