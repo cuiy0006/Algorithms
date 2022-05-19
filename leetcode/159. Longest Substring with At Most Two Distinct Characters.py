6920406f6577adbcc9d278c5a1155acc62846bdb
Yao Cui <cuiy0006@gmail.com>
2022-05-18 23:56:03 -0400

    Update 159. Longest Substring with At Most Two Distinct Characters.py

--- leetcode/159. Longest Substring with At Most Two Distinct Characters.py
+++ leetcode/159. Longest Substring with At Most Two Distinct Characters.py
@@ -1,26 +1,26 @@
-class Solution(object):
-    def lengthOfLongestSubstringTwoDistinct(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
+class Solution:
+    def lengthOfLongestSubstringTwoDistinct(self, s: str) -> int:
+        k = 2
         dic = {}
-        cnt = 0
-        i, j = 0, 0
-        maxlen = 0
+        
+        res = 0
+        i = 0
+        j = 0
         while j < len(s):
             if s[j] in dic:
-                if dic[s[j]] == 0:
-                    cnt += 1
+                dic[s[j]] += 1
             else:
-                cnt += 1
-            dic[s[j]] = dic.get(s[j], 0) + 1
-            if cnt > 2:
-                maxlen = max(maxlen, j-i)
-            while cnt > 2:
-                dic[s[i]] -= 1
-                if dic[s[i]] == 0:
-                    cnt -= 1
-                i += 1
+                dic[s[j]] = 1
+                if len(dic) == k+1:
+                    res = max(res, j-i)
+                    while i < j:
+                        dic[s[i]] -= 1
+                        if dic[s[i]] == 0:
+                            del dic[s[i]]
+                            i += 1
+                            break
+                        i += 1
             j += 1
-        return max(maxlen, j - i)
+        
+        res = max(res, j-i)
+        return res
