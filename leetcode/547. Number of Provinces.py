bc6d7fe7d09c1bccfffdc2dc04365f38839957b4
Yao Cui <cuiy0006@gmail.com>
2022-05-23 00:36:15 -0400

    Update 547. Number of Provinces.py

--- leetcode/547. Number of Provinces.py
+++ leetcode/547. Number of Provinces.py
@@ -21,3 +21,36 @@ class Solution:
             helper(city)
         
         return res
+
+    
+    
+    
+ class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        n = len(isConnected)
+        seen = [False] * n
+        
+        graph = defaultdict(list)
+        for city in range(n):
+            for other, connected in enumerate(isConnected[city]):
+                if connected:
+                    graph[city].append(other)
+
+
+        def helper(city):
+            if seen[city]:
+                return
+            
+            seen[city] = True
+            
+            for other in graph[city]:
+                helper(other)
+
+        res = 0
+        for city in range(n):
+            if seen[city]:
+                continue
+            res += 1
+            helper(city)
+        
+        return res
