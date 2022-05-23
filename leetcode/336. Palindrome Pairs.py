134f94a51fdb4a851be14f4334cc0f81255eb50f
Yao Cui <cuiy0006@gmail.com>
2022-05-23 02:09:04 -0400

    Update 336. Palindrome Pairs.py

--- leetcode/336. Palindrome Pairs.py
+++ leetcode/336. Palindrome Pairs.py
@@ -1,27 +1,45 @@
 class Solution:
-    def palindromePairs(self, words):
-        """
-        :type words: List[str]
-        :rtype: List[List[int]]
-        """
-        dic = {} #word -> index
+    def palindromePairs(self, words: List[str]) -> List[List[int]]:
+        # case 1: CAT TAC
+        # case 2: CAT SOLOSTAC
+        # case 3: CATSOLOS TAC
+        
+        # case 2
+        def get_valid_suffix(word):
+            res = []
+            for i in range(len(word)):
+                if word[:i+1] == word[:i+1][::-1]:
+                    res.append(word[i+1:])
+            return res
+        
+        # case 3
+        def get_valid_prefix(word):
+            res = []
+            for i in range(len(word)):
+                if word[i:] == word[i:][::-1]:
+                    res.append(word[:i])
+            return res
+        
+        word_to_idx = {word: i for i, word in enumerate(words)}
+        
+        res = []
+        
         for i, word in enumerate(words):
-            dic[word] = i
+            # case 1
+            rev = word[::-1]
+            if rev in word_to_idx and word_to_idx[rev] != i:
+                res.append([i, word_to_idx[rev]])
             
-        res = set()
-        for i, word in enumerate(words):
-            for j in range(len(word)+1):
-                sub = word[:j]
-                panlindrome = word[j:]
-                if panlindrome == panlindrome[::-1]:
-                    reverse_sub = sub[::-1]
-                    if reverse_sub in dic and reverse_sub != word:
-                        res.add((i, dic[reverse_sub]))
-                        
-                sub = word[j:]
-                panlindrome = word[:j]
-                if panlindrome == panlindrome[::-1]:
-                    reverse_sub = sub[::-1]
-                    if reverse_sub in dic and reverse_sub != word:
-                        res.add((dic[reverse_sub], i))
-        return [[i, j] for i, j in res]
+            # case 2
+            for suffix in get_valid_suffix(word):
+                rev_suffix = suffix[::-1]
+                if rev_suffix in word_to_idx:
+                    res.append([word_to_idx[rev_suffix], i])
+            
+            # case 3
+            for prefix in get_valid_prefix(word):
+                rev_prefix = prefix[::-1]
+                if rev_prefix in word_to_idx:
+                    res.append([i, word_to_idx[rev_prefix]])
+        
+        return res
