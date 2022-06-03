fa68dedf8380f6bd7f178b79a88aadf74661a60a
Yao Cui <cuiy0006@gmail.com>
2022-06-02 21:53:36 -0400

    Update 388. Longest Absolute File Path.py

--- leetcode/388. Longest Absolute File Path.py
+++ leetcode/388. Longest Absolute File Path.py
@@ -1,32 +1,18 @@
 class Solution:
-    def lengthLongestPath(self, input):
-        """
-        :type input: str
-        :rtype: int
-        """
-        maxLen = 0
-        dic = {}
-        depth = 0
-        curr = ''
-        i = 0
-        while i < len(input):
-            c = input[i]
-            if c != '\n' and i != len(input)-1:
-                curr += c
-            else:
-                if i == len(input) - 1:
-                    curr += c
-                if '.' in curr:
-                    length = len(curr)
-                    for j in range(depth):
-                        length += dic[j] + 1
-                    maxLen = max(maxLen, length)
-                else:
-                    dic[depth] = len(curr)
-                curr = ''
-                depth = 0
-                while i + 1 < len(input) and input[i+1] == '\t':
-                    i += 1
-                    depth += 1
-            i += 1
-        return maxLen
+    def lengthLongestPath(self, input: str) -> int:
+        stack = []
+        lst = input.split('\n')
+        res = 0
+        
+        for i in range(len(lst)):
+            path = lst[i]
+            path_lst = path.split('\t')
+            level = len(path_lst)-1
+            while len(stack) > level:
+                stack.pop()
+            stack.append(path_lst[-1])
+            if '.' in path:
+                s = '/'.join(stack)
+                res = max(res, len(s))
+        
+        return res
