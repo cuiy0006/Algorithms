33cf0d8009a82bd2bea4c0d4c8a9d961f805a661
Yao Cui <cuiy0006@gmail.com>
2022-06-12 12:58:47 -0400

    Update 670. Maximum Swap.py

--- leetcode/670. Maximum Swap.py
+++ leetcode/670. Maximum Swap.py
@@ -26,3 +26,52 @@ class Solution(object):
                 lst[index], lst[i] = lst[i], lst[index]
                 break
         return reduce(lambda x,y: x*10 + y, lst)
+
+    
+    
+    
+    
+    
+from heapq import heappush, heappop
+from collections import deque
+
+class Node:
+    def __init__(self, digit):
+        self.digit = digit
+        self.q = deque()
+
+    def __lt__(self, other):
+        return self.digit > other.digit
+    
+        
+class Solution:
+    def maximumSwap(self, num: int) -> int:
+        h = []
+        lst = [int(digit) for digit in str(num)]
+        dic = {}
+        
+        for i, digit in enumerate(lst):
+            if digit not in dic:
+                dic[digit] = Node(digit)
+            dic[digit].q.append(i)
+
+        for node in dic.values():
+            heappush(h, node)
+        
+        for i, digit in enumerate(lst):
+            if digit == h[0].digit:
+                dic[digit].q.popleft()
+                if len(dic[digit].q) == 0:
+                    del dic[digit]
+                    heappop(h)
+            else:
+                idx = h[0].q.pop()
+                lst[i], lst[idx] = lst[idx], lst[i]
+                break
+        
+        res = 0
+        for digit in lst:
+            res = res * 10 + digit
+        return res
+            
+        
