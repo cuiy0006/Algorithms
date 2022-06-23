3e18e1e2e7a72fd9acec775a026c83870d6a1a4b
Yao Cui <cuiy0006@gmail.com>
2022-06-23 00:16:31 -0400

    Update 27. Remove Element.py

--- leetcode/27. Remove Element.py
+++ leetcode/27. Remove Element.py
@@ -1,4 +1,25 @@
 class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        i = 0
+        j = 0
+        
+        while i < len(nums):
+            while i < len(nums) and nums[i] == val:
+                i += 1
+            
+            if i == len(nums):
+                break
+            
+            nums[j] = nums[i]
+            j += 1
+            i += 1
+        return j
+                
+                
+
+
+
+class Solution:
     def removeElement(self, nums, val):
         """
         :type nums: List[int]
