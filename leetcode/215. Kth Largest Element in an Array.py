14e27000df7fa1c9ea70189b1e97d90aadf8554d
Yao Cui <cuiy0006@gmail.com>
2022-06-26 01:50:27 -0400

    Update 215. Kth Largest Element in an Array.py

--- leetcode/215. Kth Largest Element in an Array.py
+++ leetcode/215. Kth Largest Element in an Array.py
@@ -14,28 +14,35 @@ class Solution:
     
  class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        def quicksort(nums, l, r):
-            if l >= r:
+        def quick_select(nums, l, r, k):
+            if l == k or l >= r:
                 return
+
+            pivot_idx = random.randint(l, r)
+            pivot = nums[pivot_idx]
+            nums[pivot_idx], nums[l] = nums[l], nums[pivot_idx]
             
-            pivot = nums[l]
-            left = l + 1
-            right = r
-            while left < right:
-                while left < right and nums[left] <= pivot:
-                    left += 1
-                    
-                while left < right and nums[right] >= pivot:
-                    right -= 1
-                    
-                nums[left], nums[right] = nums[right], nums[left]
+            i = l+1
+            j = r
             
-            if nums[left] > pivot:
-                left -= 1
+            while i < j:
+                while i < j and nums[i] >= pivot:
+                    i += 1
+                while i < j and nums[j] <= pivot:
+                    j -= 1
+                nums[i], nums[j] = nums[j], nums[i]
+
+            if i < len(nums) and nums[i] < pivot:
+                i -= 1
+            if i < len(nums):
+                nums[l], nums[i] = nums[i], nums[l]
             
-            nums[left], nums[l] = nums[l], nums[left]
-            quicksort(nums, l, left - 1)
-            quicksort(nums, left + 1, r)
-        
-        quicksort(nums, 0, len(nums) - 1)
-        return nums[len(nums) - k]
+            if i+1 <= k:
+                quick_select(nums, i+1, r, k)
+            else:
+                quick_select(nums, l, i-1, k)
+    
+        quick_select(nums, 0, len(nums)-1, k)
+
+        return nums[k-1]
+                
