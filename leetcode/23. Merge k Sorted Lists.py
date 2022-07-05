d1b6ccf6603967908f99f3ef959ffa7325173447
Yao Cui <cuiy0006@gmail.com>
2022-07-04 22:42:29 -0400

    Update 23. Merge k Sorted Lists.py

--- leetcode/23. Merge k Sorted Lists.py
+++ leetcode/23. Merge k Sorted Lists.py
@@ -32,48 +32,39 @@ class Solution:
             q.append(head.next)
         return q.popleft()
              
-
+        
+        
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-from collections import deque
+
+from heapq import heappush, heappop
+
+class Node:
+    def __init__(self, listnode):
+        self.listnode = listnode
+    
+    def __lt__(self, other):
+        return self.listnode.val < other.listnode.val
 
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        def merge_two(root1, root2):
-            node = root = ListNode()
-            node1 = root1
-            node2 = root2
-            
-            while node1 and node2:
-                if node1.val < node2.val:
-                    node.next = node1
-                    node1 = node1.next
-                else:
-                    node.next = node2
-                    node2 = node2.next
-                    
-                node = node.next
-            
-            while node1:
-                node.next = node1
-                node = node.next
-                node1 = node1.next
-            
-            while node2:
-                node.next = node2
-                node = node.next
-                node2 = node2.next
-            return root.next
+        h = []
+        root = ListNode()
+        curr = root
         
-        q = deque(lists)
+        for lst in lists:
+            if lst is not None:
+                heappush(h, Node(lst))
         
-        while len(q) > 1:
-            node1 = q.popleft()
-            node2 = q.popleft()
-            
-            q.append(merge_two(node1, node2))
+        while len(h) != 0:
+            node = heappop(h)
+            listnode = node.listnode
+            curr.next = listnode
+            curr = curr.next
+            if listnode.next is not None:
+                heappush(h, Node(listnode.next))
         
-        return q.popleft() if len(q) > 0 else None
+        return root.next
