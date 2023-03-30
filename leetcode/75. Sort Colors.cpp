1e175a5b42b2e285450cb416c23a082ff5510fcf
Yao Cui <cuiy0006@gmail.com>
2023-03-29 23:16:28 -0400

    Update 75. Sort Colors.cpp

--- leetcode/75. Sort Colors.cpp
+++ leetcode/75. Sort Colors.cpp
@@ -32,3 +32,32 @@ public:
         }
     }
 };
+
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int num_zero = 0;
+        int num_one = 0;
+        for (auto num : nums) {
+            if (num == 0) {
+                num_zero++;
+            } else if (num == 1) {
+                num_one++;
+            }
+        }
+        int i = 0;
+        while (i < num_zero) {
+            nums[i] = 0;
+            i++;
+        }
+        while (i < num_zero + num_one) {
+            nums[i] = 1;
+            i++;
+        }
+        while (i < nums.size()) {
+            nums[i] = 2;
+            i++;
+        }
+    }
+};
