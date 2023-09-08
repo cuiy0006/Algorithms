aebb66b414114521220b94d82ad9b95859e53619
Yao Cui <cuiy0006@gmail.com>
2023-09-08 14:57:09 -0400

    Update 213. House Robber II.cpp

--- leetcode/213. House Robber II.cpp
+++ leetcode/213. House Robber II.cpp
@@ -4,6 +4,40 @@ public:
         if (nums.size() == 1) {
             return nums[0];
         }
+
+        return max(
+                rob_partial(nums, 1, nums.size()-1),
+                nums[0] + rob_partial(nums, 2, nums.size()-2)
+            );
+    }
+
+private:
+    int rob_partial(vector<int>& nums, int start_idx, int end_idx) {
+        if (start_idx > end_idx) {
+            return 0;
+        }
+        int this_rob = nums[start_idx];
+        int this_not_rob = 0;
+
+        for (size_t i = start_idx + 1; i <= end_idx; i++) {
+            int last_rob = this_rob;
+            int last_not_rob = this_not_rob;
+            this_rob = last_not_rob + nums[i];
+            this_not_rob = max(last_rob, last_not_rob);
+        }
+        return max(this_rob, this_not_rob);
+    }
+};
+
+
+
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if (nums.size() == 1) {
+            return nums[0];
+        }
         return max(
             robhelper(vector<int>(nums.begin(), nums.end()-1)),
             robhelper(vector<int>(nums.begin()+1, nums.end()))
