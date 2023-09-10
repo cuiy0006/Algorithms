77767b9037b588a4b58c724ffd9f732742446cdb
Yao Cui <cuiy0006@gmail.com>
2023-09-10 19:18:45 -0400

    Update 376. Wiggle Subsequence.cpp

--- leetcode/376. Wiggle Subsequence.cpp
+++ leetcode/376. Wiggle Subsequence.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
     int wiggleMaxLength(vector<int>& nums) {
-        size_t n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(2, 0));
-        
-        dp[0][0] = 1; // last element of an increasing subsequence
-        dp[0][1] = 1; // last element of a decreasing subsequence
-        
-        for (size_t i = 1; i < n; ++i) {
+        // last element of an increasing sequence
+        // last element of a decreasing sequence
+        vector<int> dp(2, 1);
+
+        for (size_t i = 1; i < nums.size(); i++) {
+            int up_count = dp[0];
+            int down_count = dp[1];
+
             if (nums[i] > nums[i-1]) {
-                dp[i][0] = max(dp[i-1][0], dp[i-1][1] + 1);
+                dp[0] = max(up_count, down_count + 1);
             } else if (nums[i] < nums[i-1]) {
-                dp[i][1] = max(dp[i-1][0] + 1, dp[i-1][1]);
-            } else {
-                dp[i][0] = dp[i-1][0];
-                dp[i][1] = dp[i-1][1];
-            }
+                dp[1] = max(down_count, up_count + 1);
+            } 
         }
-        
-        return max(dp[n-1][0], dp[n-1][1]);
+
+        return max(dp[0], dp[1]);
     }
 };
