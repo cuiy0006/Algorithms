be63d0f5f3998a87069b506518157ec1fcaa6319
Yao Cui <cuiy0006@gmail.com>
2023-09-10 17:03:57 -0400

    Update 309. Best Time to Buy and Sell Stock with Cooldown.cpp

--- leetcode/309. Best Time to Buy and Sell Stock with Cooldown.cpp
+++ leetcode/309. Best Time to Buy and Sell Stock with Cooldown.cpp
@@ -1,6 +1,37 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
+        // not hold do nothing -> not hold
+        // not hold buy        -> hold
+        // hold sell           -> not hold
+        // hold do nothing     -> hold
+
+        vector<int> dp(4, 0);
+        dp[1] = -prices[0];
+        dp[3] = -prices[0];
+
+        for (size_t i = 1; i < prices.size(); i++) {
+            int n_h_d_n = dp[0];
+            int n_h_b = dp[1];
+            int h_s = dp[2];
+            int h_d_n = dp[3];
+
+            dp[0] = max(n_h_d_n, h_s);
+            dp[1] = n_h_d_n - prices[i];
+            dp[2] = max(n_h_b + prices[i], h_d_n + prices[i]);
+            dp[3] = max(h_d_n, n_h_b);
+        }
+
+        return max({dp[0], dp[1], dp[2], dp[3]});
+    }
+};
+
+
+
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
         size_t n = prices.size();
         vector<vector<int>> dp(n, vector<int>(3, 0));
         
