cc498144f722f30473816903c200422ad7b2374f
Yao Cui <cuiy0006@gmail.com>
2023-09-10 21:25:30 -0400

    Update 276. Paint Fence.cpp

--- leetcode/276. Paint Fence.cpp
+++ leetcode/276. Paint Fence.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
     int numWays(int n, int k) {
+        if (n == 1) {
+            return k;
+        }
+        vector<int> dp(n);
+        dp[0] = k;
+        dp[1] = k * k;
+        for (size_t i = 2; i < n; i++) {
+            dp[i] = dp[i - 2] * (k - 1) + dp[i - 1] * (k - 1);
+        }
+        return dp[n - 1];
+    }
+};
+
+
+
+
+class Solution {
+public:
+    int numWays(int n, int k) {
         unordered_map<int, int> dp;
         return totalWays(n, k, dp);
     }
