2cad9c1d59e298dab456ac7e483e32a8f502ef35
Yao Cui <cuiy0006@gmail.com>
2025-02-20 00:07:11 -0500

    Update 360. Sort Transformed Array.py

--- leetcode/360. Sort Transformed Array.py
+++ leetcode/360. Sort Transformed Array.py
@@ -1,5 +1,36 @@
 class Solution:
     def sortTransformedArray(self, nums: List[int], a: int, b: int, c: int) -> List[int]:
+        def f(num):
+            return a * num * num + b * num + c
+
+        if a == 0:
+            res = [f(num) for num in nums]
+            if b < 0:
+                res.reverse()
+            return res
+        maxmin = max if a > 0 else min
+        i = 0
+        j = len(nums)-1
+        res = []
+        while i <= j:
+            left = f(nums[i])
+            right = f(nums[j])
+            val = maxmin(left, right)
+            res.append(val)
+            if left == val:
+                i += 1
+            else:
+                j -= 1
+        if a > 0:
+            res.reverse()
+        return res
+
+
+
+
+
+class Solution:
+    def sortTransformedArray(self, nums: List[int], a: int, b: int, c: int) -> List[int]:
         if a == 0:
             res = [b * num + c for num in nums]
             if b < 0:
