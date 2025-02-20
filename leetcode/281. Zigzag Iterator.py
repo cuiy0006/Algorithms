b55053c4c4b1afd23f187ef42b30f93da13c7361
Yao Cui <cuiy0006@gmail.com>
2025-02-20 01:44:24 -0500

    Update 281. Zigzag Iterator.py

--- leetcode/281. Zigzag Iterator.py
+++ leetcode/281. Zigzag Iterator.py
@@ -1,5 +1,34 @@
 class ZigzagIterator:
     def __init__(self, v1: List[int], v2: List[int]):
+        self.v = [v1, v2]
+        self.i = 0
+        self.idx = [0, 0]
+
+    def next(self) -> int:
+        res = self.v[self.i][self.idx[self.i]]
+        self.idx[self.i] += 1
+        self.i = (self.i+1) % 2
+        return res
+
+    def hasNext(self) -> bool:
+        if self.idx[self.i] < len(self.v[self.i]):
+            return True
+        j = self.i
+        self.i = (self.i+1) % 2
+        while self.i != j:
+            if self.idx[self.i] < len(self.v[self.i]):
+                return True
+            self.i = (self.i+1)%2
+        return False
+
+# Your ZigzagIterator object will be instantiated and called as such:
+# i, v = ZigzagIterator(v1, v2), []
+# while i.hasNext(): v.append(i.next())
+
+
+
+class ZigzagIterator:
+    def __init__(self, v1: List[int], v2: List[int]):
         self.v1 = v1
         self.v2 = v2
         self.v1_idx = 0
