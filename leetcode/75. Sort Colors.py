661558eb13c58d86b53681290877a0dc9623c20b
Yao Cui <cuiy0006@gmail.com>
2025-02-20 18:49:45 -0500

    Update 75. Sort Colors.py

--- leetcode/75. Sort Colors.py
+++ leetcode/75. Sort Colors.py
@@ -3,22 +3,16 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        left = 0
-        right = len(nums) - 1
-        while left < right:
-            while left < right and nums[left] == 0:
-                left += 1
-            while left < right and nums[right] != 0:
-                right -= 1
-            
-            nums[left], nums[right] = nums[right], nums[left]
-        
-        right = len(nums) - 1
-        while left < right:
-            while left < right and nums[left] == 1:
-                left += 1
-            while left < right and nums[right] == 2:
-                right -= 1
-                
-            nums[left], nums[right] = nums[right], nums[left]
-            
+        p0 = 0
+        p2 = len(nums)-1
+        i = 0
+        while i <= p2:
+            if nums[i] == 0:
+                nums[p0], nums[i] = nums[i], nums[p0]
+                p0 += 1
+                i += 1
+            elif nums[i] == 2:
+                nums[p2], nums[i] = nums[i], nums[p2]
+                p2 -= 1
+            else:
+                i += 1
