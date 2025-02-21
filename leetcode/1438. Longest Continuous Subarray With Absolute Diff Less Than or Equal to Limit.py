6c6a7f7f1121bf4d450cf606188f5a004020539e
Yao Cui <cuiy0006@gmail.com>
2025-02-21 00:11:30 -0500

    Update 1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit.py

--- leetcode/1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit.py
+++ leetcode/1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit.py
@@ -1,3 +1,37 @@
+class Solution:
+    def longestSubarray(self, nums: List[int], limit: int) -> int:
+        min_dq = deque()
+        max_dq = deque()
+        i = 0
+        j = 0
+        res = 0
+        while j < len(nums):
+            while len(min_dq) != 0 and abs(nums[min_dq[0]]-nums[j]) > limit:
+                idx = min_dq.popleft()
+                while len(max_dq) != 0 and max_dq[0] <= idx:
+                    max_dq.popleft()
+                i = max(i, idx+1)
+            while len(max_dq) != 0 and abs(nums[max_dq[0]]-nums[j]) > limit:
+                idx = max_dq.popleft()
+                while len(min_dq) != 0 and min_dq[0] <= idx:
+                    min_dq.popleft()
+                i = max(i, idx+1)
+            res = max(res, j-i+1)
+
+            while len(max_dq) != 0 and nums[max_dq[-1]] < nums[j]:
+                max_dq.pop()
+            max_dq.append(j)
+            while len(min_dq) != 0 and nums[min_dq[-1]] > nums[j]:
+                min_dq.pop()
+            min_dq.append(j)
+            j += 1
+        return res
+
+
+
+
+
+
 from sortedcontainers import SortedDict
 
 class Solution:
