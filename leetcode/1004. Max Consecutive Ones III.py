8ce751658a450d394798576b7e802651efc75621
Yao Cui <cuiy0006@gmail.com>
2025-02-20 21:38:59 -0500

    Update 1004. Max Consecutive Ones III.py

--- leetcode/1004. Max Consecutive Ones III.py
+++ leetcode/1004. Max Consecutive Ones III.py
@@ -1,5 +1,29 @@
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
+        dq = deque()
+        i = 0
+        j = 0
+        res = 0
+
+        while j < len(nums):
+            if nums[j] == 0:
+                if k == 0:
+                    j += 1
+                    i = j
+                else:
+                    if len(dq) == k:
+                        i = dq.popleft()+1
+                    dq.append(j)
+                    j += 1
+            else:
+                j += 1
+            res = max(res, j-i)
+        return res
+
+
+
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
         flip = 0
         i = 0
         j = 0
