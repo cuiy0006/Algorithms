cbdba4ff717bf6f950ec4f45777632983bf4ec76
Yao Cui <cuiy0006@gmail.com>
2025-02-22 00:10:52 -0500

    Update 395. Longest Substring with At Least K Repeating Characters.py

--- leetcode/395. Longest Substring with At Least K Repeating Characters.py
+++ leetcode/395. Longest Substring with At Least K Repeating Characters.py
@@ -1,19 +1,22 @@
 class Solution:
     def longestSubstring(self, s: str, k: int) -> int:
         def helper(start, end):
-            if start > end:
-                return 0
-            
-            res = 0
-            counts = [0] * 26
+            dic = {}
             for i in range(start, end+1):
-                counts[ord(s[i])-ord('a')] += 1
-            
-            i = j = start
-            while j <= end:
-                if counts[ord(s[j])-ord('a')] < k:
+                if s[i] not in dic:
+                    dic[s[i]] = 0
+                dic[s[i]] += 1
+
+            i = start
+            res = 0
+            for j in range(start, end+1):
+                if dic[s[j]] < k:
                     res = max(res, helper(i, j-1))
-                    i = j + 1
-                j += 1
-            
+                    i = j+1
+
+            if i == start:
+                return end-start+1
+            res = max(res, helper(i, end))
             return res
+
+        return helper(0, len(s)-1)
