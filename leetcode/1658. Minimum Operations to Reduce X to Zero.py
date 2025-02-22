81f046040969961585a43604270addc3d612fbc7
Yao Cui <cuiy0006@gmail.com>
2025-02-21 20:53:24 -0500

    Update 1658. Minimum Operations to Reduce X to Zero.py

--- leetcode/1658. Minimum Operations to Reduce X to Zero.py
+++ leetcode/1658. Minimum Operations to Reduce X to Zero.py
@@ -1,23 +1,19 @@
-
-
-
 class Solution:
     def minOperations(self, nums: List[int], x: int) -> int:
-        presum = [0]
+        presums = [0]
         curr = 0
         dic = {}
         for i, num in enumerate(nums):
             curr += num
-            presum.append(curr)
+            presums.append(curr)
 
         target = curr - x
         res = -1
-        for i, val in enumerate(presum):
-            if val not in dic:
-                dic[val] = i
-            if (val - target) in dic:
-                res = max(res, i - dic[val-target])
+        for i, presum in enumerate(presums):
+            if presum not in dic:
+                dic[presum] = i
+            if (presum - target) in dic:
+                res = max(res, i - dic[presum-target])
         if res == -1:
             return -1
         return len(nums) - res
-
