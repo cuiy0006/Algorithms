f6b713edcc93606c5135cecf92cd0d777c8e04dc
Yao Cui <cuiy0006@gmail.com>
2025-02-21 21:25:53 -0500

    Update 219. Contains Duplicate II.py

--- leetcode/219. Contains Duplicate II.py
+++ leetcode/219. Contains Duplicate II.py
@@ -1,14 +1,9 @@
 class Solution:
-    def containsNearbyDuplicate(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: bool
-        """
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
         dic = {}
         for i, num in enumerate(nums):
-            if num in dic:
-                if i - dic[num] <= k:
-                    return True
+            if num in dic and i - dic[num] <= k:
+                return True
             dic[num] = i
+        
         return False
