54e767e03f8866b7a88d90fdc172eb3774bd7e2a
Yao Cui <cuiy0006@gmail.com>
2025-02-22 00:40:01 -0500

    Update 560. Subarray Sum Equals K.py

--- leetcode/560. Subarray Sum Equals K.py
+++ leetcode/560. Subarray Sum Equals K.py
@@ -1,24 +1,15 @@
-class Solution(object):
-    def subarraySum(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: int
-        """
-        dic = {}
-        curr = 0
-        cnt = 0
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        dic = {0:1}
+        res = 0
+        presum = 0
         for num in nums:
-            curr += num
-                
-            if curr == k:
-                cnt += 1
-                
-            if curr - k in dic:
-               cnt += dic[curr - k]
-               
-            if curr in dic:
-                dic[curr] += 1
-            else:
-                dic[curr] = 1
-        return cnt
+            presum += num
+            target = presum - k
+            if target in dic:
+                res += dic[target]
+            if presum not in dic:
+                dic[presum] = 0
+            dic[presum] += 1
+        
+        return res
