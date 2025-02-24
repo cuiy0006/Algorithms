4ac819ba1cf402982fe20c8fb8d206844a5244d3
Yao Cui <cuiy0006@gmail.com>
2025-02-23 21:49:52 -0500

    Update 852. Peak Index in a Mountain Array.cpp

--- leetcode/852. Peak Index in a Mountain Array.cpp
+++ leetcode/852. Peak Index in a Mountain Array.cpp
@@ -1,11 +1,16 @@
-class Solution {
-public:
-    int peakIndexInMountainArray(vector<int>& A) {
-        for(int i = 0; i < A.size() - 1; ++i){
-            if(A[i] > A[i + 1]){
-                return i;
-            }    
-        }
-        return -1;
-    }
-};
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        l = 0
+        r = len(arr)-1
+
+        while l < r:
+            print(l, r)
+            mid = (l+r)//2
+            if arr[mid] > arr[mid-1] and arr[mid] < arr[mid+1]:
+                l = mid+1
+            elif arr[mid] < arr[mid-1] and arr[mid] > arr[mid+1]:
+                r = mid
+            else:
+                return mid
+        
+        return -1
