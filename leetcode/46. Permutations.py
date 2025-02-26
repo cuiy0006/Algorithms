56e804cb6daf0f13d5e286ebfc388a4a9fce1f8e
Yao Cui <cuiy0006@gmail.com>
2025-02-25 21:25:26 -0500

    Update 46. Permutations.py

--- leetcode/46. Permutations.py
+++ leetcode/46. Permutations.py
@@ -1,18 +1,15 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         res = []
-        
-        def get_permute(idx, curr):
-            if idx == len(nums):
-                res.append(curr[:])
+        def helper(start):
+            if start == len(nums):
+                res.append(nums[:])
                 return
             
-            for i in range(idx, len(nums)):
-                curr.append(nums[i])
-                nums[i], nums[idx] = nums[idx], nums[i]
-                get_permute(idx+1, curr)
-                nums[i], nums[idx] = nums[idx], nums[i]
-                curr.pop()
-            
-        get_permute(0, [])
+            for i in range(start, len(nums)):
+                nums[i], nums[start] = nums[start], nums[i]
+                helper(start+1)
+                nums[i], nums[start] = nums[start], nums[i]
+        
+        helper(0)
         return res
