cc90d6f4752b3d85b14588c903910a5e19af4f6d
Yao Cui <cuiy0006@gmail.com>
2025-02-26 01:54:34 -0500

    Update 90. Subsets II.py

--- leetcode/90. Subsets II.py
+++ leetcode/90. Subsets II.py
@@ -1,49 +1,14 @@
-#recursive
-class Solution(object):
-    def subsetsWithDup(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
         res = []
-        def helper(index, lst):
-            if index <= len(nums):
-                res.append(lst[:])
-            if index == len(nums):
-                return
-            
-            i = index
-            while i < len(nums):
-                lst.append(nums[i])
-                helper(i+1, lst)
-                lst.pop()
-                i += 1
-                while i < len(nums) and nums[i] == nums[i-1]:
-                    i += 1
+        def helper(start, curr):
+            res.append(curr[:])
+            for i in range(start, len(nums)):
+                if i != start and nums[i] == nums[i-1]:
+                    continue
+                curr.append(nums[i])
+                helper(i+1, curr)
+                curr.pop()
         helper(0, [])
         return res
-        
-        
-#iterative
-class Solution(object):
-    def subsetsWithDup(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
-        nums.sort()
-        res = [[]]
-        size = 0
-        for i, num in enumerate(nums):
-            start = 0
-            if i != 0 and nums[i] == nums[i-1]:
-                start = size
-            size = len(res)
-            for j in range(start, size):
-                lst = res[j][:]
-                lst.append(nums[i])
-                res.append(lst)
-        return res
-        
-        
