01d99c4d1a52009b2be27143dea579556cfb13da
Yao Cui <cuiy0006@gmail.com>
2025-02-25 23:51:47 -0500

    Update 77. Combinations.py

--- leetcode/77. Combinations.py
+++ leetcode/77. Combinations.py
@@ -1,16 +1,15 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         res = []
-
-        def helper(m, curr):
+        def helper(start, curr):
             if len(curr) == k:
                 res.append(curr[:])
                 return
-            
-            for i in range(m, n+1):
+
+            for i in range(start, n+1):
                 curr.append(i)
                 helper(i+1, curr)
                 curr.pop()
-                
+
         helper(1, [])
         return res
