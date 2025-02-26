a9cc5bd203badb65e1ba671d46f97e0b8dde7426
Yao Cui <cuiy0006@gmail.com>
2025-02-26 01:30:14 -0500

    Update 78. Subsets.py

--- leetcode/78. Subsets.py
+++ leetcode/78. Subsets.py
@@ -1,3 +1,16 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        def helper(start, curr):
+            res.append(curr[:])
+            for i in range(start, len(nums)):
+                curr.append(nums[i])
+                helper(i+1, curr)
+                curr.pop()
+        helper(0, [])
+        return res
+
+
 class Solution(object):
     def subsets(self, nums):
         """
