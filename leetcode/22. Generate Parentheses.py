6e593b357c0724abc6a693aad159e34f18f880b3
Yao Cui <cuiy0006@gmail.com>
2025-02-26 22:35:11 -0500

    Update 22. Generate Parentheses.py

--- leetcode/22. Generate Parentheses.py
+++ leetcode/22. Generate Parentheses.py
@@ -1,19 +1,17 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        if n == 0:
-            return []
         res = []
-        def generate(left, right, curr):
+        def helper(left, right, curr):
             if left == 0 and right == 0:
                 res.append(curr)
                 return
             
             if left == right:
-                generate(left - 1, right, curr + '(')
+                helper(left-1, right, curr+'(')
             elif left < right:
-                if left > 0:
-                    generate(left - 1, right, curr + '(')
-                generate(left, right - 1, curr + ')')
-                    
-        generate(n, n, '')
+                if left != 0:
+                    helper(left-1, right, curr+'(')
+                helper(left, right-1, curr+')')
+        
+        helper(n, n, '')
         return res
