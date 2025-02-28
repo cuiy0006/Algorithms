0c35b367c5bd6afaebfa861703c1b52c1e15844d
Yao Cui <cuiy0006@gmail.com>
2025-02-27 23:02:04 -0500

    Update 117. Populating Next Right Pointers in Each Node II.py

--- leetcode/117. Populating Next Right Pointers in Each Node II.py
+++ leetcode/117. Populating Next Right Pointers in Each Node II.py
@@ -1,77 +1,31 @@
-# Definition for binary tree with next pointer.
-# class TreeLinkNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-#         self.next = None
-from collections import deque
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
 class Solution:
-    # @param root, a tree link node
-    # @return nothing
-    def connect(self, root):
-        if root == None:
-            return
+    def connect(self, root: 'Node') -> 'Node':
+        if root is None:
+            return None
+        
         q = deque([root])
         while len(q) != 0:
-            cnt = len(q)
-            while cnt != 0:
-                node = q.popleft()
-                cnt -= 1
-                if cnt == 0:
-                    node.next = None
+            size = len(q)
+            curr = None
+            for _ in range(size):
+                if curr is None:
+                    curr = q.popleft()
                 else:
-                    node.next = q[0]
-                if node.left != None:
-                    q.append(node.left)
-                if node.right != None:
-                    q.append(node.right)
-
-
-
-
-
-# Definition for binary tree with next pointer.
-# class TreeLinkNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-#         self.next = None
-
-from collections import deque
-class Solution:
-    # @param root, a tree link node
-    # @return nothing
-    def connect(self, root):
-        if root is None:
-            return
-        curr = root
-        next_curr = None
-        next_head = None
-        
-        while curr is not None:
-            while curr is not None:
+                    curr.next = q.popleft()
+                    curr = curr.next
                 if curr.left is not None:
-                    if next_curr is None:
-                        next_head = curr.left
-                        next_curr = curr.left
-                    else:
-                        next_curr.next = curr.left
-                        next_curr = next_curr.next
-                    
+                    q.append(curr.left)
                 if curr.right is not None:
-                    if next_curr is None:
-                        next_head = curr.right
-                        next_curr = curr.right
-                    else:
-                        next_curr.next = curr.right
-                        next_curr = next_curr.next
-                
-                curr = curr.next
-            curr = next_head
-            next_head = None
-            next_curr = None
-
-
-
+                    q.append(curr.right)
+        
+        return root
