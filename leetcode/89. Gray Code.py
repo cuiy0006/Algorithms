87d0c6393d81c7cd328241c65e339e2d49554204
Yao Cui <cuiy0006@gmail.com>
2025-02-27 20:17:40 -0500

    Update 89. Gray Code.py

--- leetcode/89. Gray Code.py
+++ leetcode/89. Gray Code.py
@@ -17,15 +17,6 @@ class Solution(object):
 
 class Solution:
     def grayCode(self, n: int) -> List[int]:
-        def str_to_num(s):
-            num = 0
-            n = 1
-            for i in range(len(s)-1, -1, -1):
-                if s[i] == '1':
-                    num += n
-                n *= 2
-            return num
-
         def helper(curr, seen, curr_lst):
             if curr in seen:
                 return None
@@ -35,21 +26,23 @@ class Solution:
 
             if len(curr_lst) == 2**n:
                 ones = 0
-                for c in curr:
-                    if c == '1':
+                tmp = curr
+                while tmp != 0:
+                    if tmp & 1 == 1:
                         ones += 1
                         if ones > 1:
                             return None
+                    tmp = tmp >> 1
                 return curr_lst[:]
 
-            for i in range(len(curr)):
-                r = '0' if curr[i] == '1' else '1' 
-                res = helper(curr[:i] + r + curr[i+1:], seen, curr_lst)
+            k = 0
+            tmp = 1
+            while k < n:
+                res = helper(curr^tmp, seen, curr_lst)
                 if res is not None:
                     return res
+                tmp = tmp<<1
             seen.remove(curr)
             curr_lst.pop()
             return None
 
-        res = helper('0'*n, set(), [])
-        return [str_to_num(s) for s in res]
