96d2cba81944f8caefee161d1e2f57eff54f5521
Yao Cui <cuiy0006@gmail.com>
2025-03-03 22:45:59 -0500

    Update 59. Spiral Matrix II.py

--- leetcode/array/59. Spiral Matrix II.py
+++ leetcode/array/59. Spiral Matrix II.py
@@ -1,36 +1,36 @@
 class Solution:
     def generateMatrix(self, n: int) -> List[List[int]]:
-        dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-        matrix = [[0 for _ in range(n)] for _ in range(n)]
-        matrix[0][0] = 1
-        x_min = 0
-        y_min = 0
-        x_max = n-1
-        y_max = n-1
+        res = [[None for _ in range(n)] for _ in range(n)]
 
-        i = 0
-        j = 0
-        n = 0
+        directions = ((0,1),(1,0),(0,-1),(-1,0))
+        left = 0
+        right = n-1
+        up = 0
+        down = n-1
+
+        d = 0
+        x = 0
+        y = 0
         curr = 2
-        while x_min <= x_max and y_min <= y_max:
-            d = dirs[n]
-            while True:
-                i0 = i + d[0]
-                j0 = j + d[1]
-                if i0 >= x_min and i0 <= x_max and j0 >= y_min and j0 <= y_max:
-                    i = i0
-                    j = j0
-                    matrix[i][j] = curr
-                    curr += 1
-                else:
-                    break
-            if n == 0:
-                x_min += 1
-            elif n == 1:
-                y_max -= 1
-            elif n == 2:
-                x_max -= 1
+        res[0][0] = 1
+        while True:
+            x0, y0 = directions[d]
+            while x+x0 >= up and x+x0 <= down and y+y0 >= left and y+y0 <= right:
+                x += x0
+                y += y0
+                res[x][y] = curr
+                curr += 1      
+
+            if d == 0:
+                up += 1
+            elif d == 1:
+                right -= 1
+            elif d == 2:
+                down -= 1
             else:
-                y_min += 1
-            n = (n + 1) % 4
-        return matrix
+                left += 1
+            if left > right or up > down:
+                break
+            d = (d+1)%4
+        
+        return res
