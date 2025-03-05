d802b766a9cd235c7628f1dc6bc44b0ce534c88c
Yao Cui <cuiy0006@gmail.com>
2025-03-04 22:51:33 -0500

    Create 1099. Two Sum Less Than K.py

---
+++ leetcode/1099. Two Sum Less Than K.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSumLessThanK(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        l = 0
+        r = len(nums)-1
+        res = -1
+        while l < r:
+            total = nums[l] + nums[r]
+            if total < k:
+                res = max(res, total)
+                l += 1
+            else:
+                r -= 1
+        return res
