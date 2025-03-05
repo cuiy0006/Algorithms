89c1e7ff5964d92163cac202950cc676d870014f
Yao Cui <cuiy0006@gmail.com>
2025-03-04 22:04:27 -0500

    Update 18. 4Sum.py

--- leetcode/18. 4Sum.py
+++ leetcode/18. 4Sum.py
@@ -2,32 +2,31 @@ class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         res = []
         nums.sort()
-        i = 0
-        
-        while i < len(nums):
-            j = len(nums) - 1
-            while j > i:
-                left = target - nums[i] - nums[j]
-                m = i + 1
-                n = j - 1
-                while m < n:
-                    total = nums[m] + nums[n]
-                    if total == left:
-                        res.append([nums[i], nums[m], nums[n], nums[j]])
-                        m += 1
-                        while m < n and nums[m] == nums[m-1]:
-                            m += 1
-                    elif total > left:
-                        n -= 1
+        d = 3
+        while d < len(nums):
+            while d < len(nums)-1 and nums[d] == nums[d+1]:
+                d += 1
+            c = 2
+            while c < d:
+                while c < d-1 and nums[c] == nums[c+1]:
+                    c += 1
+                a = 0
+                b = c-1
+                while a < b:
+                    total = nums[a] + nums[b] + nums[c] + nums[d]
+                    if total < target:
+                        a += 1
+                    elif total > target:
+                        b -= 1
                     else:
-                        m += 1
-                j -= 1
-                while j > i and nums[j] == nums[j+1]:
-                    j -= 1
-            
-            i += 1
-            while i < len(nums) and nums[i] == nums[i-1]:
-                i += 1
-        
+                        res.append([nums[a], nums[b], nums[c], nums[d]])
+                        a += 1
+                        b -= 1
+                        while a < b and nums[a] == nums[a-1]:
+                            a += 1
+                        while a < b and nums[b] == nums[b+1]:
+                            b -= 1
+                c += 1
+            d += 1
         return res
-                    
+
