467294a32db24151d3ed8f2fa894e228984cdc6f
Yao Cui <cuiy0006@gmail.com>
2025-03-05 01:01:44 -0500

    Update 240. Search a 2D Matrix II.py

--- leetcode/240. Search a 2D Matrix II.py
+++ leetcode/240. Search a 2D Matrix II.py
@@ -1,18 +1,14 @@
-class Solution(object):
-    def searchMatrix(self, matrix, target):
-        """
-        :type matrix: List[List[int]]
-        :type target: int
-        :rtype: bool
-        """
-        if len(matrix) == 0:
-            return False
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m = len(matrix)
+        n = len(matrix[0])
+        
         i = 0
-        j = len(matrix[0])-1
-        while i < len(matrix) and j >= 0:
-            if target == matrix[i][j]:
+        j = n-1
+        while i < m and j >= 0:
+            if matrix[i][j] == target:
                 return True
-            elif target > matrix[i][j]:
+            elif matrix[i][j] < target:
                 i += 1
             else:
                 j -= 1
