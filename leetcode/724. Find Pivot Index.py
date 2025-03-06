677925fe6c8699d9f0b3d469973eba0f310d4367
Yao Cui <cuiy0006@gmail.com>
2025-03-06 02:59:19 -0500

    Update 724. Find Pivot Index.py

--- leetcode/724. Find Pivot Index.py
+++ leetcode/724. Find Pivot Index.py
@@ -1,27 +1,14 @@
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
-        sum_from_right = [-sys.maxsize] * len(nums)
-        
-        curr_sum = 0
-        for i in range(len(nums)-1, -1, -1):
-            curr_sum += nums[i]
-            sum_from_right[i] = curr_sum
-            
-        curr_sum = 0
+        presum = []
+        curr = 0
+        for num in nums:
+            curr += num
+            presum.append(curr)
+
         for i in range(len(nums)):
-            if i == 0:
-                left = 0
-            else:
-                left = curr_sum
-            
-            if i == len(nums)-1:
-                right = 0
-            else:
-                right = sum_from_right[i+1]
-                
+            left = presum[i-1] if i != 0 else 0
+            right = presum[-1]-presum[i]
             if left == right:
                 return i
-            curr_sum += nums[i]
-        
         return -1
-        
