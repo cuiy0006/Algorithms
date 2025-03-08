79f9ab1391d46fcefb5db351f7ec76cd240eaaf0
Yao Cui <cuiy0006@gmail.com>
2025-03-07 20:56:06 -0500

    Update 1109. Corporate Flight Bookings.py

--- leetcode/array/1109. Corporate Flight Bookings.py
+++ leetcode/array/1109. Corporate Flight Bookings.py
@@ -13,3 +13,25 @@ class Solution:
                 curr += dic[flight]
             res.append(curr)
         return res
+
+
+
+class Solution:
+    def corpFlightBookings(self, bookings: List[List[int]], n: int) -> List[int]:
+        dic = {}
+        for [first, last, seat] in bookings:
+            if first not in dic:
+                dic[first] = 0
+            dic[first] += seat
+            if last+1 not in dic:
+                dic[last+1] = 0
+            dic[last+1] -= seat
+        
+        res = [None] * n
+        curr = 0
+        for i in range(n):
+            if i+1 in dic:
+                curr += dic[i+1]
+            res[i] = curr
+        return res
+
