7508d2b6474b31fd1a7a5884782b4fcfa4a2481a
Yao Cui <cuiy0006@gmail.com>
2025-03-11 19:37:24 -0400

    Update 622. Design Circular Queue.py

--- leetcode/622. Design Circular Queue.py
+++ leetcode/622. Design Circular Queue.py
@@ -48,3 +48,62 @@ class MyCircularQueue:
 # param_4 = obj.Rear()
 # param_5 = obj.isEmpty()
 # param_6 = obj.isFull()
+
+
+
+class MyCircularQueue:
+
+    def __init__(self, k: int):
+        self.start = -1
+        self.rear = -1
+        self.arr = [None] * k
+
+    def enQueue(self, value: int) -> bool:
+        if self.isFull():
+            return False
+        if self.isEmpty():
+            self.start = 0
+            self.rear = 0
+            self.arr[0] = value
+        else:
+            self.rear = (self.rear+1)%len(self.arr)
+            self.arr[self.rear] = value
+        return True
+
+    def deQueue(self) -> bool:
+        if self.isEmpty():
+            return False
+        is_last_element = (self.start == self.rear)
+        # self.arr[self.start] == None
+        self.start = (self.start+1)%len(self.arr)
+        if is_last_element:
+            self.start = -1
+            self.rear = -1
+
+        return True
+
+    def Front(self) -> int:
+        if self.isEmpty():
+            return -1
+        return self.arr[self.start]
+
+    def Rear(self) -> int:
+        if self.isEmpty():
+            return -1
+        return self.arr[self.rear]
+
+    def isEmpty(self) -> bool:
+        return self.rear == -1
+
+    def isFull(self) -> bool:
+        return (self.rear+1)%len(self.arr) == self.start
+
+
+# Your MyCircularQueue object will be instantiated and called as such:
+# obj = MyCircularQueue(k)
+# param_1 = obj.enQueue(value)
+# param_2 = obj.deQueue()
+# param_3 = obj.Front()
+# param_4 = obj.Rear()
+# param_5 = obj.isEmpty()
+# param_6 = obj.isFull()
