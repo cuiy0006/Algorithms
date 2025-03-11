519d5817f863dadf2c468b5bf83c0c9ae7a5d17a
Yao Cui <cuiy0006@gmail.com>
2025-03-10 23:25:24 -0400

    Update 150. Evaluate Reverse Polish Notation.py

--- leetcode/150. Evaluate Reverse Polish Notation.py
+++ leetcode/150. Evaluate Reverse Polish Notation.py
@@ -1,6 +1,26 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         stack = []
+        dic = {
+            '+': lambda x,y: x+y,
+            '-': lambda x,y: x-y,
+            '*': lambda x,y: x*y,
+            '/': lambda x,y: int(x/y)
+        }
+        for token in tokens:
+            if token in dic:
+                num1 = stack.pop()
+                num2 = stack.pop()
+                num = dic[token](num2, num1)
+                stack.append(num)
+            else:
+                stack.append(int(token))
+        return stack[0]
+
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
         for token in tokens:
             if token in {'+', '-', '*', '/'}:
                 num2 = stack.pop()
