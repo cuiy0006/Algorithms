4445150698b024fc5d6d3eec0529045295252e4e
Yao Cui <cuiy0006@gmail.com>
2025-03-15 14:08:18 -0400

    Update 623. Add One Row to Tree.py

--- leetcode/623. Add One Row to Tree.py
+++ leetcode/623. Add One Row to Tree.py
@@ -1,4 +1,35 @@
 # Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def addOneRow(self, root: Optional[TreeNode], val: int, depth: int) -> Optional[TreeNode]:
+        if depth == 1:
+            return TreeNode(val, root, None)
+
+        def traverse(node, d):
+            if node is None:
+                return
+            if d == depth-1:
+                left = node.left
+                right = node.right
+                node.left = TreeNode(val)
+                node.right = TreeNode(val)
+                node.left.left = left
+                node.right.right = right
+                return
+            traverse(node.left, d+1)
+            traverse(node.right, d+1)
+        
+        traverse(root, 1)
+        return root
+
+
+
+
+# Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, x):
 #         self.val = x
