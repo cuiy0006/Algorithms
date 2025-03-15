8686cd91773fd738a866ab6897a7a394f590f956
Yao Cui <cuiy0006@gmail.com>
2025-03-14 20:24:34 -0400

    Update 98. Validate Binary Search Tree.py

--- leetcode/98. Validate Binary Search Tree.py
+++ leetcode/98. Validate Binary Search Tree.py
@@ -1,46 +1,23 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
-class Solution(object):
-    def isValidBST(self, root):
-        """
-        :type root: TreeNode
-        :rtype: bool
-        """
-        def helper(top, bottom, node):
-            if node is None:
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        last = -sys.maxsize
+        def inorder(root):
+            if root is None:
                 return True
-            if (node.left is None or node.val > node.left.val and node.left.val > bottom) and \
-            (node.right is None or node.val < node.right.val and node.right.val < top):
-                return helper(node.val, bottom, node.left) and helper(top, node.val, node.right)
-            return False
-        return helper(sys.maxsize, -sys.maxsize, root)
-                
+            if not inorder(root.left):
+                return False
+            nonlocal last
+            if root.val <= last:
+                return False
+            last = root.val
+            if not inorder(root.right):
+                return False
+            return True
+        return inorder(root)
 
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
-class Solution(object):
-    def isValidBST(self, root):
-        """
-        :type root: TreeNode
-        :rtype: bool
-        """
-        last = [-sys.maxsize]
-        def helper(node):
-            if node == None:
-                return True
-            left = helper(node.left)
-            mid = node.val > last[0]
-            last[0] = node.val
-            right = helper(node.right)
-            return left and mid and right
-        return helper(root)
