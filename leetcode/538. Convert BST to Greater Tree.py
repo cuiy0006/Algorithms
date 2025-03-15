b32e7abe35a14b2580ff75ea54859fe8fe0c1d8e
Yao Cui <cuiy0006@gmail.com>
2025-03-14 20:17:52 -0400

    Update 538. Convert BST to Greater Tree.py

--- leetcode/538. Convert BST to Greater Tree.py
+++ leetcode/538. Convert BST to Greater Tree.py
@@ -1,40 +1,18 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-from collections import deque
-class Solution(object):
-    def convertBST(self, root):
-        """
-        :type root: TreeNode
-        :rtype: TreeNode
-        """
-        if root == None:
-            return None
-        lst = []
-        self.inorder(root, lst)
-        for i in list(range(len(lst) - 1))[::-1]:
-            lst[i] += lst[i + 1]
-        node = TreeNode(0)
-        self.helper(root, node, deque(lst))
-        return node
-        
-        
-    def inorder(self, node, lst):
-        if node == None:
-            return
-        self.inorder(node.left, lst)
-        lst.append(node.val)
-        self.inorder(node.right, lst)
-        
-    def helper(self, root, node, lst):
-        if root.left != None:
-            node.left = TreeNode(0)
-            self.helper(root.left, node.left, lst)
-        node.val = lst[0]
-        lst.popleft()
-        if root.right != None:
-            node.right = TreeNode(0)
-            self.helper(root.right, node.right, lst)
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def convertBST(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        def traverse(node, presum):
+            if node is None:
+                return presum
+            
+            presum = traverse(node.right, presum)
+            node.val += presum
+            return traverse(node.left, node.val)
+
+        traverse(root, 0)
+        return root
