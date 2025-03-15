01ea511ac3247e5a5e693f016254c62106a0fda6
Yao Cui <cuiy0006@gmail.com>
2025-03-15 18:49:09 -0400

    Update 386. Lexicographical Numbers.py

--- leetcode/386. Lexicographical Numbers.py
+++ leetcode/386. Lexicographical Numbers.py
@@ -17,3 +17,22 @@ class Solution(object):
                 while curr % 10 == 0:
                     curr = curr // 10
         return res
+
+
+class Solution:
+    def lexicalOrder(self, n: int) -> List[int]:
+        res = []
+        def traverse(curr):
+            if len(res) == n:
+                return
+            if curr > n:
+                return
+            if curr != 0:
+                res.append(curr)
+            start = 0 if curr != 0 else 1
+            for i in range(start, 10):
+                traverse(curr*10+i)
+
+        traverse(0)
+        return res
+
