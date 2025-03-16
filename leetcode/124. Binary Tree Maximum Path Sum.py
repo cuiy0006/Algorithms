ab1fca7a8d0917823311ca403ca884d619b0f178
Yao Cui <cuiy0006@gmail.com>
2025-03-16 01:02:45 -0400

    Update 124. Binary Tree Maximum Path Sum.py

--- leetcode/124. Binary Tree Maximum Path Sum.py
+++ leetcode/124. Binary Tree Maximum Path Sum.py
@@ -1,28 +1,30 @@
 # Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
-import sys
-
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def maxPathSum(self, root: TreeNode) -> int:
-        if not root:
-            return 0
-        max_val = [-sys.maxsize]
-        def helper(node):
-            if not node:
-                return 0
-            left_max = helper(node.left)
-            right_max = helper(node.right)
-            node_max = node.val
-            if left_max > 0:
-                node_max += left_max
-            if right_max > 0:
-                node_max += right_max
-            max_val[0] = max(max_val[0], node_max)
-            return max(left_max, right_max, 0) + node.val
-        helper(root)
-        return max_val[0]
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        res = root.val
+        def traverse(node, presum):
+            if node is None:
+                return -sys.maxsize
+            
+            presum += node.val
+            left = traverse(node.left, presum)
+            right = traverse(node.right, presum)
+
+            max_sum = node.val
+            if left > presum:
+                max_sum += left-presum
+            if right > presum:
+                max_sum += right-presum
+            nonlocal res
+            res = max(res, max_sum)
+
+            return max(presum, left, right)
+        
+        traverse(root, 0)
+        return res
+
