84ece347ec58338b97ce36c7416f6fc1bca29416
Yao Cui <cuiy0006@gmail.com>
2025-03-18 18:22:10 -0400

    Update 99. Recover Binary Search Tree.py

--- leetcode/99. Recover Binary Search Tree.py
+++ leetcode/99. Recover Binary Search Tree.py
@@ -5,6 +5,38 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    def recoverTree(self, root: Optional[TreeNode]) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        lst = []
+        def inorder(node):
+            if node is None:
+                return
+            inorder(node.left)
+            lst.append(node)
+            inorder(node.right)
+        inorder(root)
+        sort = list(sorted(lst, key=lambda node:node.val))
+        for i in range(len(lst)):
+            if lst[i].val != sort[i].val:
+                lst[i].val, sort[i].val = sort[i].val, lst[i].val
+                break
+        return root
+        
+
+
+
+
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
     def recoverTree(self, root: TreeNode) -> None:
         """
         Do not return anything, modify root in-place instead.
