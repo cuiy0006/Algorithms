bd226ff79d63537e12a8bdd1b382d858da662244
Yao Cui <cuiy0006@gmail.com>
2025-03-17 21:15:58 -0400

    Update 1339. Maximum Product of Splitted Binary Tree.py

--- leetcode/1339. Maximum Product of Splitted Binary Tree.py
+++ leetcode/1339. Maximum Product of Splitted Binary Tree.py
@@ -6,31 +6,20 @@
 #         self.right = right
 class Solution:
     def maxProduct(self, root: Optional[TreeNode]) -> int:
-        node_to_sum = {}
-        
-        def get_node_sum(node):
+        def getsum(node):
             if node is None:
                 return 0
-            
-            left_val = get_node_sum(node.left)
-            right_val = get_node_sum(node.right)
-            val = node.val + left_val + right_val
-            node_to_sum[node] = val
-            return val
-        
-        get_node_sum(root)
-        
-        total = node_to_sum[root]
+            return getsum(node.left)+getsum(node.right)+node.val
+        total = getsum(root)
+
         res = 0
-        def preorder(node):
+        def traverse(node):
             if node is None:
-                return
-            
+                return 0
+            curr = node.val + traverse(node.left) + traverse(node.right)
             nonlocal res
-            res = max(res, (total-node_to_sum[node])*node_to_sum[node])
-            preorder(node.left)
-            preorder(node.right)
-        
-        preorder(root)
-        return res % 1000000007
+            res = max(res, (total-curr)*curr)
+            return curr
+        traverse(root)
+        return res % (10**9 + 7)
             
