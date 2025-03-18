7f3e1ffd6140b428b083983a9fc6f3ed3ea15377
Yao Cui <cuiy0006@gmail.com>
2025-03-17 21:40:11 -0400

    Update 606. Construct String from Binary Tree.py

--- leetcode/606. Construct String from Binary Tree.py
+++ leetcode/606. Construct String from Binary Tree.py
@@ -1,3 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def tree2str(self, root: Optional[TreeNode]) -> str:
+        def traverse(node):
+            if node is None:
+                return None
+            left = traverse(node.left)
+            right = traverse(node.right)
+            if left is None and right is None:
+                return str(node.val)
+            if left is None and right is not None:
+                return str(node.val) + '()(' + right + ')'
+            if left is not None and right is None:
+                return str(node.val) + '(' + left + ')'
+            return str(node.val) + '(' + left + ')(' + right + ')'
+
+        return traverse(root) 
+
+
+
 class Solution:
     def tree2str(self, t):
         """
