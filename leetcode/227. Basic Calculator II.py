bc54b58e36fef684041a0ea19b78649f0455ac26
Yao Cui <cuiy0006@gmail.com>
2025-03-23 18:18:10 -0400

    Update 227. Basic Calculator II.py

--- leetcode/227. Basic Calculator II.py
+++ leetcode/227. Basic Calculator II.py
@@ -1,67 +1,27 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        sign = '+'
-        curr = ''
-        stack = []
-        s = s.strip()
-        for i, c in enumerate(s):
-            if c.isdigit():
-                curr += c
-            if not c.isdigit() or i == len(s) - 1:
-                if curr == '' and curr == '-':
-                    sign = c
-                    continue
-                if c == ' ':
-                    continue
-                if sign == '+':
-                    stack.append(int(curr))
-                elif sign == '-':
-                    stack.append(-int(curr))
-                elif sign == '*':
-                    stack.append(stack.pop() * int(curr))
-                elif sign == '/':
-                    num = stack.pop()
-                    isPositive = num > 0
-                    res = abs(num) // int(curr)
-                    if not isPositive:
-                        res = -res
-                    stack.append(res)
-                curr = ''
-                sign = c
-        return sum(stack)
-
-    
-class Solution:
-    def calculate(self, s: str) -> int:
-        origin = s
-        s = ''
-        for c in origin:
+        tmp = ''
+        for c in s:
             if c != ' ':
-                s += c
-        
-        stack = []
+                tmp += c
+        s = tmp
         if s[0] != '+' and s[0] != '-':
             s = '+' + s
-        
-        ops = ('+', '-', '*', '/')
-        
         i = 0
+        stack = []
+        ops = {'+', '-', '*', '/'}
         while i < len(s):
-            j = i + 1
-            if s[j] == '+' or s[j] == '-':
-                j += 1
+            j = i+1
             while j < len(s) and s[j] not in ops:
                 j += 1
-            curr = int(s[i+1:j])
+            num = int(s[i+1:j])
             if s[i] == '+':
-                stack.append(curr)
+                stack.append(num)
             elif s[i] == '-':
-                stack.append(-curr)
+                stack.append(-num)
             elif s[i] == '*':
-                stack[-1] *= curr
+                stack.append(stack.pop() * num)
             else:
-                stack[-1] = int(stack[-1] / curr)
+                stack.append(int(stack.pop() / num))
             i = j
-        
-        return sum(stack)
-                
+        return sum(stack) 
