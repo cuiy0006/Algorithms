440f9529db22d512ba4b362ff2efc7b3c297b8b0
Yao Cui <cuiy0006@gmail.com>
2025-03-23 18:37:15 -0400

    Update 772. Basic Calculator III.py

--- leetcode/772. Basic Calculator III.py
+++ leetcode/772. Basic Calculator III.py
@@ -1,42 +1,43 @@
 class Solution:
-    def calculate(self, s: str) -> str:
-        ops = ('+', '-', '*', '/')
-        
+    def calculate(self, s: str) -> int:
         def evaluate(s):
-            stack = []
+            tmp = ''
+            for c in s:
+                if c != ' ':
+                    tmp += c
+            s = tmp
             if s[0] != '+' and s[0] != '-':
                 s = '+' + s
             i = 0
+            stack = []
+            ops = {'+', '-', '*', '/'}
             while i < len(s):
                 j = i+1
                 if s[j] == '+' or s[j] == '-':
                     j += 1
                 while j < len(s) and s[j] not in ops:
                     j += 1
-                curr = int(s[i+1:j])
+                num = int(s[i+1:j])
                 if s[i] == '+':
-                    stack.append(curr)
+                    stack.append(num)
                 elif s[i] == '-':
-                    stack.append(-curr)
+                    stack.append(-num)
                 elif s[i] == '*':
-                    stack[-1] *= curr
+                    stack[-1] = stack[-1]*num
                 else:
-                    stack[-1] = int(stack[-1]/curr)
+                    stack[-1] = int(stack[-1]/num)
                 i = j
-            return str(sum(stack))
-            
+            return sum(stack)
         
-        stack = []
         curr = ''
+        stack = []
         for c in s:
             if c == '(':
                 stack.append(curr)
                 curr = ''
             elif c == ')':
-                curr = evaluate(curr)
-                if len(stack) != 0:
-                    curr = stack.pop() + curr
+                curr = stack.pop() + str(evaluate(curr))
             else:
                 curr += c
-
-        return int(evaluate(curr))
+        print(curr)
+        return evaluate(curr)
