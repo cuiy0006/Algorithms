0d4b58089465ba6e3e11ca195109371896447e7b
Yao Cui <cuiy0006@gmail.com>
2025-03-27 01:03:07 -0400

    Update 1631. Path With Minimum Effort.py

--- leetcode/1631. Path With Minimum Effort.py
+++ leetcode/1631. Path With Minimum Effort.py
@@ -1,3 +1,33 @@
+class Solution:
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+        m = len(heights)
+        n = len(heights[0])
+        start = (0, 0)
+        end = (m-1, n-1)
+
+        dirs = ((1,0),(-1,0),(0,1),(0,-1))
+        dic = {(i, j): sys.maxsize for i in range(m) for j in range(n)}
+        h = [(0, 0, 0)]
+        while len(h) != 0:
+            effort, x, y = heappop(h)
+            if effort >= dic[(x, y)]:
+                continue
+            dic[(x, y)] = effort
+            if (x, y) == end:
+                print(dic)
+                return effort
+            for x0,y0 in dirs:
+                x1 = x+x0
+                y1 = y+y0
+                if x1 < 0 or x1 > m-1 or y1 < 0 or y1 > n-1:
+                    continue
+                heappush(h, (max(effort, abs(heights[x][y]-heights[x1][y1])), x1, y1))
+        
+        return -1
+
+
+
+
 from heapq import heappush, heappop
 
 class Solution:
