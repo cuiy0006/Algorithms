18e3e26f4df15976f6cff3950af756f1ea05223e
Yao Cui <cuiy0006@gmail.com>
2025-03-27 00:46:32 -0400

    Update 1514. Path with Maximum Probability.py

--- leetcode/graph/1514. Path with Maximum Probability.py
+++ leetcode/graph/1514. Path with Maximum Probability.py
@@ -1,23 +1,30 @@
 class Solution:
     def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start_node: int, end_node: int) -> float:
-        graph = defaultdict(list)
-        for i, [a, b] in enumerate(edges):
+        graph = {}
+        for i in range(len(edges)):
+            a = edges[i][0]
+            b = edges[i][1]
+            if a not in graph:
+                graph[a] = []
+            if b not in graph:
+                graph[b] = []
             graph[a].append((b, succProb[i]))
             graph[b].append((a, succProb[i]))
-        h = [] # [(prob from start, node)]
-        heappush(h, (-1, start_node))
-        visited = {}
+        
+        h = [(-1, start_node)]
+        dic = {node:0 for node in range(n)}
 
         while len(h) != 0:
             prob, node = heappop(h)
             prob = -prob
-            if node in visited and prob <= visited[node]:
+            if prob <= dic[node]:
                 continue
-            visited[node] = prob
+            dic[node] = prob
+            if node == end_node:
+                return prob
+            if node not in graph:
+                continue
             for neighbor, p in graph[node]:
-                heappush(h, (-p*prob, neighbor))
-        
-        if end_node not in visited:
-            return 0
-        return visited[end_node]
+                heappush(h, (-prob*p, neighbor))
 
+        return dic[end_node]
