478ae6ef7425ae82a92aa369ae03706c1db7fb64
Yao Cui <cuiy0006@gmail.com>
2025-03-29 15:28:33 -0400

    Update 1079. Letter Tile Possibilities.py

--- leetcode/1079. Letter Tile Possibilities.py
+++ leetcode/1079. Letter Tile Possibilities.py
@@ -1,5 +1,29 @@
 class Solution:
     def numTilePossibilities(self, tiles: str) -> int:
+        tiles = list(tiles)
+        cnt = 0
+        def dfs(idx):
+            if idx != 0:
+                nonlocal cnt
+                cnt += 1
+            if idx == len(tiles):
+                return
+            seen = set()
+            for i in range(idx, len(tiles)):
+                if tiles[i] in seen:
+                    continue
+                seen.add(tiles[i])
+                tiles[i], tiles[idx] = tiles[idx], tiles[i]
+                dfs(idx+1)
+                tiles[i], tiles[idx] = tiles[idx], tiles[i]
+        
+        dfs(0)
+        return cnt
+
+
+
+class Solution:
+    def numTilePossibilities(self, tiles: str) -> int:
         dic = {}
         for tile in tiles:
             if tile not in dic:
