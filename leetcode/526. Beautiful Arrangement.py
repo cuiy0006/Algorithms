e92af6062718cbe7dbc6df251399f8ef84a52665
Yao Cui <cuiy0006@gmail.com>
2025-03-29 14:50:25 -0400

    Update 526. Beautiful Arrangement.py

--- leetcode/526. Beautiful Arrangement.py
+++ leetcode/526. Beautiful Arrangement.py
@@ -6,10 +6,10 @@ class Solution:
                 return 1
             res = 0
             for i in range(idx, len(curr)):
-                curr[i], curr[idx] = curr[idx], curr[i]
-                if curr[idx] % (idx+1) == 0 or (idx+1) % curr[idx] == 0:
-                   res += get_count(idx+1, curr)
-                curr[i], curr[idx] = curr[idx], curr[i]
+                if curr[i] % (idx+1) == 0 or (idx+1) % curr[i] == 0:
+                    curr[i], curr[idx] = curr[idx], curr[i]
+                    res += get_count(idx+1, curr)
+                    curr[i], curr[idx] = curr[idx], curr[i]
             return res
         
         return get_count(0, [i for i in range(1, n+1)])
