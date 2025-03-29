78706128574ea45e6711fd3e44de5064868516c3
Yao Cui <cuiy0006@gmail.com>
2025-03-28 23:28:12 -0400

    Update 547. Number of Provinces.py

--- leetcode/547. Number of Provinces.py
+++ leetcode/547. Number of Provinces.py
@@ -1,6 +1,32 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
         n = len(isConnected)
+        parent = {city:city for city in range(n)}
+        def find_ancestor(node):
+            if parent[node] == node:
+                return node
+            parent[node] = find_ancestor(parent[node])
+            return parent[node]
+        
+        for a in range(n):
+            for b in range(a):
+                if isConnected[a][b] == 1:
+                    anc_a = find_ancestor(a)
+                    anc_b = find_ancestor(b)
+                    parent[anc_a] = anc_b
+
+        s = set()
+        for city in range(n):
+            ancestor = find_ancestor(city)
+            if ancestor not in s:
+                s.add(ancestor)
+        return len(s)
+
+
+
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        n = len(isConnected)
         seen = [False] * n
         
         def helper(city):
