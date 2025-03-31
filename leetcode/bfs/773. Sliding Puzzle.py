aa31f044ec2b04e2baecf0149e58c907dc2cc8a5
Yao Cui <cuiy0006@gmail.com>
2025-03-30 22:03:59 -0400

    Update 773. Sliding Puzzle.py

--- leetcode/bfs/773. Sliding Puzzle.py
+++ leetcode/bfs/773. Sliding Puzzle.py
@@ -40,4 +40,50 @@ class Solution:
 
 
 
+class Solution:
+    def slidingPuzzle(self, board: List[List[int]]) -> int:
+        def get_hash(board):
+            curr = 0
+            for i in range(2):
+                for j in range(3):
+                    curr = curr * 10 + board[i][j]
+            return curr
+
+        def get_copy(board):
+            copy = [[board[i][j] for j in range(3)] for i in range(2)]
+            return copy
+
+        dirs = ((1,0),(-1,0),(0,1),(0,-1))
+        end = get_hash([[1,2,3], [4,5,0]])
+        seen = set()
+        q = deque([board])
+        d = 0
+        while len(q) != 0:
+            size = len(q)
+            for _ in range(size):
+                board = q.popleft()
+                board_hash = get_hash(board)
+                if board_hash == end:
+                    return d
+                if board_hash in seen:
+                    continue
+                seen.add(board_hash)
+                x0 = None
+                y0 = None
+                for i in range(2):
+                    for j in range(3):
+                        if board[i][j] == 0:
+                            x0 = i
+                            y0 = j
+
+                for direction in dirs:
+                    x1 = x0 + direction[0]
+                    y1 = y0 + direction[1]
+                    if x1 < 0 or x1 > 1 or y1 < 0 or y1 > 2:
+                        continue
+                    copy = get_copy(board)
+                    copy[x0][y0], copy[x1][y1] = copy[x1][y1], copy[x0][y0]
+                    q.append(copy)
+            d += 1
+        return -1
 
