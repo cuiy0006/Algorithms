d8b9bfc2c779e61a58f0b88089e218f7c50608c6
Yao Cui <cuiy0006@gmail.com>
2025-03-30 21:47:26 -0400

    Update 752. Open the Lock.py

--- leetcode/bfs/752. Open the Lock.py
+++ leetcode/bfs/752. Open the Lock.py
@@ -1,42 +1,24 @@
-num_map = {
-    '0': ['1', '9'],
-    '1': ['2', '0'],
-    '2': ['3', '1'],
-    '3': ['4', '2'],
-    '4': ['5', '3'],
-    '5': ['6', '4'],
-    '6': ['7', '5'],
-    '7': ['8', '6'],
-    '8': ['9', '7'],
-    '9': ['0', '8']
-}
-
 class Solution:
     def openLock(self, deadends: List[str], target: str) -> int:
-        seen = set()
         deadends = set(deadends)
-        if target in deadends:
-            return -1
-        height = 0
         q = deque(['0000'])
-
+        seen = set()
+        d = 0
         while len(q) != 0:
             size = len(q)
             for _ in range(size):
-                curr = q.popleft()
-                if curr == target:
-                    return height
-                if curr in deadends or curr in seen:
+                lock = q.popleft()
+                if lock == target:
+                    return d
+                if lock in deadends:
                     continue
-                seen.add(curr)
-                lst = list(curr)
-                for i, c in enumerate(lst):
-                    for c1 in num_map[c]:
-                        lst[i] = c1
-                        s = ''.join(lst)
-                        q.append(s)
-                        lst[i] = c
-            height += 1
+                if lock in seen:
+                    continue
+                seen.add(lock)
+                for i in range(4):
+                    next_lock = lock[:i] + str((int(lock[i])-1+10)%10) + lock[i+1:]
+                    q.append(next_lock)
+                    next_lock = lock[:i] + str((int(lock[i])+1)%10) + lock[i+1:]
+                    q.append(next_lock)
+            d += 1
         return -1
-
-
