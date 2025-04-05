aec70a34c71e6e9ce96020ee2a3007d2568678d9
Yao Cui <cuiy0006@gmail.com>
2025-04-04 20:02:16 -0400

    Update 63. Unique Paths II.py

--- leetcode/63. Unique Paths II.py
+++ leetcode/63. Unique Paths II.py
@@ -1,5 +1,24 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        if obstacleGrid[0][0] == 1 or obstacleGrid[-1][-1] == 1:
+            return 0
+        m = len(obstacleGrid)
+        n = len(obstacleGrid[0])
+        paths = [[0 for _ in range(n)] for _ in range(m)]
+        paths[0][0] = 1
+        for i in range(m):
+            for j in range(n):
+                if obstacleGrid[i][j] == 1:
+                    continue
+                if i > 0:
+                    paths[i][j] += paths[i-1][j]
+                if j > 0:
+                    paths[i][j] += paths[i][j-1]
+        return paths[-1][-1]
+
+
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
         m = len(obstacleGrid)
         n = len(obstacleGrid[0])
         
