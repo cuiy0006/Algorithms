0ae6219e03e69a5e29bb6602066bb04e83acf70d
Yao Cui <cuiy0006@gmail.com>
2025-04-05 00:25:24 -0400

    Update 1048. Longest String Chain.py

--- leetcode/1048. Longest String Chain.py
+++ leetcode/1048. Longest String Chain.py
@@ -1,5 +1,31 @@
 class Solution:
     def longestStrChain(self, words: List[str]) -> int:
+        words = set(words)
+        dic = defaultdict(list)
+        for word in words:
+            for i in range(len(word)):
+                pre = word[:i]+word[i+1:]
+                if pre in words:
+                    dic[pre].append(word)
+        chain = {}
+        def get_chain(word):
+            if word in chain:
+                return chain[word]
+            cnt = 0
+            for successor in dic[word]:
+                cnt = max(cnt, get_chain(successor))
+            chain[word] = cnt+1
+            return chain[word]
+
+        res = 0
+        for word in words:
+            res = max(res, get_chain(word))
+        return res
+
+
+
+class Solution:
+    def longestStrChain(self, words: List[str]) -> int:
         word_to_len = {'': 0}
         words.sort(key=lambda word:len(word))
         
