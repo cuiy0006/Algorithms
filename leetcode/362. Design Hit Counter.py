0502c53a46b754d9ae44c05285ee21bcb238b051
Yao Cui <cuiy0006@gmail.com>
2025-04-09 21:48:21 -0400

    Update 362. Design Hit Counter.py

--- leetcode/362. Design Hit Counter.py
+++ leetcode/362. Design Hit Counter.py
@@ -1,19 +1,21 @@
-from collections import deque
-
 class HitCounter:
 
     def __init__(self):
+        self.cnt = 0
         self.q = deque()
 
     def hit(self, timestamp: int) -> None:
-        self.q.append(timestamp)
+        self.cnt += 1
+        if len(self.q) != 0 and self.q[-1][0] == timestamp:
+            self.q[-1][1] += 1
+        else:
+            self.q.append([timestamp, 1])
 
     def getHits(self, timestamp: int) -> int:
-        after = timestamp - 300
-        while len(self.q) != 0 and self.q[0] <= after:
-            self.q.popleft()
-        
-        return len(self.q)
+        while len(self.q) != 0 and self.q[0][0]+300 <= timestamp:
+            [_, cnt] = self.q.popleft()
+            self.cnt -= cnt
+        return self.cnt
 
 
 # Your HitCounter object will be instantiated and called as such:
