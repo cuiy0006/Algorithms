eccf0530f0e91c4c5c7ffea0d3ee2ac427a0a5d8
Yao Cui <cuiy0006@gmail.com>
2025-04-10 19:45:30 -0400

    Update 881. Boats to Save People.py

--- leetcode/881. Boats to Save People.py
+++ leetcode/881. Boats to Save People.py
@@ -1,18 +1,14 @@
 class Solution:
     def numRescueBoats(self, people: List[int], limit: int) -> int:
-        cnt = 0
         people.sort()
         i = 0
-        j = len(people) - 1
+        j = len(people)-1
+        cnt = 0
         while i <= j:
-            if i == j:
-                cnt += 1
-                break
-            if people[i] + people[j] > limit:
+            if people[i]+people[j] <= limit:
+                i += 1
                 j -= 1
             else:
-                i += 1
                 j -= 1
             cnt += 1
         return cnt
-        
