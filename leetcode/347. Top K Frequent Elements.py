67d41a5d584d4187394878c1bea363725ca38fde
Yao Cui <cuiy0006@gmail.com>
2025-04-10 23:34:07 -0400

    Update 347. Top K Frequent Elements.py

--- leetcode/347. Top K Frequent Elements.py
+++ leetcode/347. Top K Frequent Elements.py
@@ -1,20 +1,24 @@
-class Solution(object):
-    def topKFrequent(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: List[int]
-        """
-        dic = {}
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        dic = defaultdict(int)
+        max_freq = 0
         for num in nums:
-            if num not in dic:
-                dic[num] = 0
             dic[num] += 1
+            max_freq = max(max_freq, dic[num])
         
-        lst = [(num, freq) for num, freq in dic.items()]
-        lst.sort(key = lambda x:-x[1])
-        lst = lst[:k]
-        return [num for num, _ in lst]
+        lst = [(freq, num) for num, freq in dic.items()]
+        buckets = [[] for _ in range(max_freq+1)]
+
+        for num, freq in dic.items():
+            buckets[freq].append(num)
+
+        res = []
+        for i in range(len(buckets)-1, -1, -1):
+            lst = buckets[i]
+            res += lst[:min(k-len(res), len(lst))]
+            if len(res) == k:
+                break
+        return res
         
         
   
