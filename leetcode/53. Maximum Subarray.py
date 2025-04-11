de87b91adae4b0e7c631efb3301bc1a2ef94dfc0
Yao Cui <cuiy0006@gmail.com>
2025-04-10 20:28:48 -0400

    Update 53. Maximum Subarray.py

--- leetcode/53. Maximum Subarray.py
+++ leetcode/53. Maximum Subarray.py
@@ -1,30 +1,10 @@
 class Solution:
-    def maxSubArray(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        maxVal = -sys.maxsize
-        curr = 0
-        for i, num in enumerate(nums):
-            curr += num
-            maxVal = max(maxVal, curr)
-            if curr < 0:
-                curr = 0
-        return maxVal
-
-    
-    
-import sys
-
-class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        res = -sys.maxsize
-        min_val = max_val = 0
+        curr = 0
+        min_val = 0
+        res = nums[0]
         for num in nums:
-            max_n = max_val + num
-            min_n = min_val + num
-            max_val = max(num, max_n, min_n)
-            min_val = min(num, max_n, min_n)
-            res = max(res, max_val)
+            curr += num
+            res = max(res, curr-min_val)
+            min_val = min(min_val, curr)
         return res
